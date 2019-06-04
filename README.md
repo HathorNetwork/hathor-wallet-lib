1a7f48ab10762b8f836bf5eef9c0e09d977f2074
Yan Martins <yan.martins@gmail.com>
2019-06-04 18:14:26 +0100

    docs: update docstring for Storage class and README

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Library used by [Hathor Wallet](https://github.com/HathorNetwork/hathor-wallet).
 ## Install
 
 `npm install @hathor/wallet-lib`
+
+# Setting storage
+
+The lib requires a storage to be passed to it. Take a look at `src/storage.js` for the methods this storage object should implement.
+```
+const hathorLib = require('@hathor/wallet-lib');
+hathorLib.storage.setStorage(storageFactory);
+```
--- src/storage.js
+++ src/storage.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright (c) Hathor Labs and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * Interface to use the storage object set by the client
+ *
+ * This storage should have the same methods as localStorage (from the browser) and has
+ * to support storaing js objects. It should handle any serialization needed.
+ *
+ * @class
+ * @name Storage
+ */
 class Storage {
   constructor() {
     if (!Storage.instance) {
@@ -7,30 +23,67 @@ class Storage {
     return Storage.instance;
   }
 
+  /**
+   * Set the underlying storage object
+   *
+   * @param {Object} myStorage The storage to be used
+   */
   setStorage(myStorage) {
     this.store = myStorage;
   }
 
+  /**
+   * Return the current value associated with the given key
+   *
+   * @param {string} key Key associated with the object
+   *
+   * @return {Object} the object stored
+   */
   getItem(key) {
     return this.store.getItem(key);
   }
 
+  /**
+   * Add the given object to the storage
+   *
+   * @param {string} key Key associated with the object
+   * @param {Object} value Object to be stored
+   */
   setItem(key, value) {
     return this.store.setItem(key, value);
   }
 
+  /**
+   * Remove the key and associated object from storage
+   *
+   * @param {string} key Key associated with the object
+   */
   removeItem(key) {
     return this.store.removeItem(key);
   }
 
+  /**
+   * Remove all key and associated objects from storage
+   */
   clear() {
     return this.store.clear();
   }
 
+  /**
+   * Return the name of the nth key in storage
+   *
+   * @param {string} n
+   *
+   * @return {Object} Object associated with nth key
+   */
   key(n) {
     return this.store.key(n);
   }
 
+  /**
+   * This method is optional and may be used to initialize the storage before the
+   * lib starts using it
+   */
   preStart() {
     return this.store.preStart();
   }
