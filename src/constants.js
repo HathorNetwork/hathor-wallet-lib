662eb939cfce0516dedd2ab2c8fa059ef72fe0ed
Pedro Ferreira <pedro@hathor.network>
2019-06-12 15:09:03 +0100

    refactor: improving constant comment

--- src/constants.js
+++ src/constants.js
@@ -116,9 +116,10 @@ export const TOKEN_MINT_MASK = 0b00000010
 export const TOKEN_MELT_MASK = 0b00000100
 
 /**
- * Token data for an authority output
+ * Token data for an authority output of the first token in a transaction.
+ * As most transactions with authority outputs have only one token, it may be directly used, as a shortcut.
  */
-export const AUTHORITY_TOKEN_DATA = 0b10000001
+export const AUTHORITY_TOKEN_DATA = TOKEN_AUTHORITY_MASK | 1
 
 /**
  * Hathor token config
