dcd039bfeaafa3c6013fba5f815f66a04c6518c6
Yan Martins <yan.martins@gmail.com>
2020-02-13 16:40:18 +0100

    fix: move xpubkey to wallet:accessData on storage (#55)

--- src/wallet.js
+++ src/wallet.js
@@ -31,6 +31,7 @@ import _ from 'lodash';
  *   . hash: string with hash of pin
  *   . words: string with encrypted words
  *   . hashPasswd: string with hash of password
+ *   . xpubkey: string with wallet xpubkey
  * - address: string with last shared address to show on screen
  * - lastSharedIndex: number with the index of the last shared address
  * - lastGeneratedIndex: number with the index of the last generated address
@@ -132,15 +133,15 @@ const wallet = {
       saltPasswd: encryptedDataWords.hash.salt,
       hashIterations: HASH_ITERATIONS,
       pbkdf2Hasher: 'sha1', // For now we are only using SHA1
+      xpubkey: privkey.xpubkey,
     }
 
     let walletData = {
       keys: {},
-      xpubkey: privkey.xpubkey,
       historyTransactions: {},
     }
 
-    storage.setItem('wallet:accessData', access);
+    this.setWalletAccessData(access);
     this.setWalletData(walletData);
 
     let promise = null;
@@ -193,6 +194,30 @@ const wallet = {
   },
 
   /**
+   * Get wallet access data already parsed from JSON
+   *
+   * @return {Object} wallet access data
+   *
+   * @memberof Wallet
+   * @inner
+   */
+  getWalletAccessData() {
+    return storage.getItem('wallet:accessData');
+  },
+
+  /**
+   * Set wallet access data
+   *
+   * @param {Object} wallet access data
+   *
+   * @memberof Wallet
+   * @inner
+   */
+  setWalletAccessData(data) {
+    storage.setItem('wallet:accessData', data);
+  },
+
+  /**
    * Load the history for each of the addresses of a new generated wallet
    * We always search until the GAP_LIMIT. If we have any history in the middle of the searched addresses
    * we search again until we have the GAP_LIMIT of addresses without any transactions
@@ -211,8 +236,9 @@ const wallet = {
       // First generate all private keys and its addresses, then get history
       let addresses = [];
       let dataJson = this.getWalletData();
+      let accessData = this.getWalletAccessData();
 
-      const xpub = HDPublicKey(dataJson.xpubkey);
+      const xpub = HDPublicKey(accessData.xpubkey);
       const stopIndex = startIndex + count;
       for (var i=startIndex; i<stopIndex; i++) {
         // Generate each key from index, encrypt and save
@@ -397,7 +423,7 @@ const wallet = {
    * @inner
    */
   hashValidation(password, hashKey, saltKey) {
-    const accessData = storage.getItem('wallet:accessData');
+    const accessData = this.getWalletAccessData();
     let hash;
     if (!(saltKey in accessData)) {
       // Old wallet, we need to validate with old method and update it to the new method
@@ -411,7 +437,7 @@ const wallet = {
       accessData['hashIterations'] = HASH_ITERATIONS;
       accessData['pbkdf2Hasher'] = 'sha1'; // For now we are only using SHA1
       // Updating access data with new hash data
-      storage.setItem('wallet:accessData', accessData);
+      this.setWalletAccessData(accessData);
       return true;
     } else {
       // Already a wallet with new hash algorithm, so only validate
@@ -437,7 +463,7 @@ const wallet = {
       return false;
     }
 
-    const accessData = storage.getItem('wallet:accessData');
+    const accessData = this.getWalletAccessData();
 
     // Get new PIN hash
     const newHash = this.hashPassword(newPin);
@@ -450,7 +476,7 @@ const wallet = {
     const encryptedData = this.encryptData(decryptedData, newPin);
     accessData['mainKey'] = encryptedData.encrypted.toString();
 
-    storage.setItem('wallet:accessData', accessData);
+    this.setWalletAccessData(accessData);
 
     return true;
   },
@@ -524,8 +550,8 @@ const wallet = {
    * @inner
    */
   generateNewAddress() {
-    const dataJson = this.getWalletData();
-    const xpub = HDPublicKey(dataJson.xpubkey);
+    const accessData = this.getWalletAccessData();
+    const xpub = HDPublicKey(accessData.xpubkey);
 
     // Get last shared index to discover new index
     const lastSharedIndex = this.getLastSharedIndex();
@@ -1121,7 +1147,7 @@ const wallet = {
    * @inner
    */
   getWalletWords(password) {
-    const accessData = storage.getItem('wallet:accessData');
+    const accessData = this.getWalletAccessData();
     return this.decryptData(accessData.words, password);
   },
 
@@ -1165,12 +1191,8 @@ const wallet = {
    */
   reloadData() {
     // Get old access data
-    const accessData = storage.getItem('wallet:accessData');
+    const accessData = this.getWalletAccessData();
     const walletData = this.getWalletData();
-
-    if (walletData === null) {
-      return Promise.reject();
-    }
 
     this.cleanWallet();
     // Restart websocket connection
@@ -1178,11 +1200,10 @@ const wallet = {
 
     let newWalletData = {
       keys: {},
-      xpubkey: walletData.xpubkey,
       historyTransactions: {},
     }
 
-    storage.setItem('wallet:accessData', accessData);
+    this.setWalletAccessData(accessData);
     this.setWalletData(newWalletData);
 
     // Load history from new server
@@ -1460,7 +1481,7 @@ const wallet = {
       // Setting last shared address, if necessary
       const candidateIndex = maxIndex + 1;
       if (candidateIndex > lastSharedIndex) {
-        const xpub = HDPublicKey(dataJson.xpubkey);
+        const xpub = HDPublicKey(this.getWalletAccessData().xpubkey);
         const key = xpub.derive(candidateIndex);
         const address = Address(key.publicKey, network.getNetwork()).toString();
         newSharedIndex = candidateIndex;
