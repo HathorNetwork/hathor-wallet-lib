3cb7026255ecbcdb944ece36145620cafbf7db1f
Pedro Ferreira <pedro@hathor.network>
2020-03-11 12:04:15 -0300

    refactor: undoing helpers storage changes

--- src/helpers.js
+++ src/helpers.js
@@ -7,7 +7,7 @@
 
 import path from 'path';
 
-import defaultStorage from './storage';
+import storage from './storage';
 import tokens from './tokens';
 import { BLOCK_VERSION, CREATE_TOKEN_TX_VERSION, DEFAULT_TX_VERSION, MERGED_MINED_BLOCK_VERSION, GENESIS_BLOCK, DECIMAL_PLACES, DEFAULT_SERVER } from './constants';
 
@@ -177,7 +177,7 @@ const helpers = {
    * @memberof Helpers
    * @inner
    */
-  getServerURL({ storage = defaultStorage }) {
+  getServerURL() {
     const server = storage.getItem('wallet:server');
     const defaultServer = storage.getItem('wallet:defaultServer');
     if (server !== null) {
