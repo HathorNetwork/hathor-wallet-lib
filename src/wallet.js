8b1712c41977a1417a6f480a24d875924caef941
Pedro Ferreira <pedro@hathor.network>
2020-07-22 19:23:20 -0300

    fix: when reloading data we should clean storage data before loading new data (#82)
    * fix: when reloading data we should clean storage data before loading new data
    
    * chore: improving change comment
    
    * refactor: reloadData method now receives an options object instead of two optional parameter

--- src/wallet.js
+++ src/wallet.js
@@ -1281,10 +1281,12 @@ const wallet = {
   /*
    * Reload data in the storage
    *
+   * @param {Object} Options object with {'connection': Connection, 'store': Store}
+   *
    * @memberof Wallet
    * @inner
    */
-  reloadData() {
+  reloadData({connection = null, store = null} = {}) {
     // Get old access data
     const accessData = this.getWalletAccessData();
     const walletData = this.getWalletData();
@@ -1302,7 +1304,7 @@ const wallet = {
     this.setWalletData(newWalletData);
 
     // Load history from new server
-    const promise = this.loadAddressHistory(0, GAP_LIMIT);
+    const promise = this.loadAddressHistory(0, GAP_LIMIT, connection, store);
     return promise;
   },
 
