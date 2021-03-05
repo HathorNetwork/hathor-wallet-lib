3f5e96275f7e77686261d8b4da0a7b320c29525c
Pedro Ferreira <pedro@hathor.network>
2021-03-05 18:06:59 -0300

    tests: fix new tests to work with node v12

--- __tests__/transaction_utils.test.js
+++ __tests__/transaction_utils.test.js
@@ -15,7 +15,6 @@ import storage from '../src/storage';
 import WebSocketHandler from '../src/WebSocketHandler';
 
 const nodeMajorVersion = process.versions.node.split('.')[0];
-console.log('#### ', nodeMajorVersion);
 
 beforeEach(() => {
   wallet.setConnection(WebSocketHandler);
