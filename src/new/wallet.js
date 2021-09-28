fe523fc5e327e6564d8ff1ccfeb613b7d6d776d7
André Abadesso <andre.abadesso@gmail.com>
2021-09-28 15:37:34 -0300

    fix: clear sensitive data from memory after wallet load

--- src/new/wallet.js
+++ src/new/wallet.js
@@ -1045,6 +1045,7 @@ class HathorWallet extends EventEmitter {
       throw "This should never happen";
     }
 
+    this.clearSensitiveData();
     this.getTokenData();
     this.serverInfo = null;
     this.walletStopped = false;
@@ -1609,6 +1610,14 @@ class HathorWallet extends EventEmitter {
   async destroyAuthority(tokenUid, type, count, options = {}) {
     const tx = await this.prepareDestroyAuthorityData(tokenUid, type, count, options);
     return this.handleSendPreparedTransaction(tx);
+  }
+
+  /**
+   * Remove sensitive data from memory
+   **/
+  clearSensitiveData() {
+    this.xpriv = undefined;
+    this.seed = undefined;
   }
 
   /**
