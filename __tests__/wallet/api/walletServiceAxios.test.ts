f4ca3f212c0f73cc760bd60052cdcbe8d13cf4e7
André Abadesso <andre.abadesso@gmail.com>
2021-12-20 13:23:22 -0300

    tests: fixed wallet service tests using config

--- __tests__/wallet/api/walletServiceAxios.test.ts
+++ __tests__/wallet/api/walletServiceAxios.test.ts
@@ -6,29 +6,33 @@ import config from '../../../src/config';
 const words = 'connect sunny silent cabin leopard start turtle tortoise dial timber woman genre pave tuna rice indicate gown draft palm collect retreat meadow assume spray';
 
 test('use testnet tx mining when network is testnet', async () => {
+  config.setWalletServiceBaseUrl('https://wallet-service.testnet.hathor.network/');
+
   const network = new Network('testnet');
   const wallet = new HathorWalletServiceWallet(words, network);
 
   const client = await axiosInstance(wallet, false);
 
-  expect(client.defaults.baseURL).toEqual("https://wallet-service.testnet.hathor.network/");
+  expect(client.defaults.baseURL).toEqual('https://wallet-service.testnet.hathor.network/');
 });
 
 test('use mainnet tx mining when network is mainnet', async () => {
+  config.setWalletServiceBaseUrl('https://wallet-service.hathor.network/');
+
   const network = new Network('mainnet');
   const wallet = new HathorWalletServiceWallet(words, network);
 
   const client = await axiosInstance(wallet, false);
 
-  expect(client.defaults.baseURL).toEqual("https://wallet-service.hathor.network/");
+  expect(client.defaults.baseURL).toEqual('https://wallet-service.hathor.network/');
 });
 
 test('use explicitly configured tx mining', async () => {
-  config.setWalletServiceBaseUrl("wallet.service.url");
+  config.setWalletServiceBaseUrl('wallet.service.url');
   const network = new Network('mainnet');
   const wallet = new HathorWalletServiceWallet(words, network);
 
   const client = await axiosInstance(wallet, false);
 
-  expect(client.defaults.baseURL).toEqual("wallet.service.url");
+  expect(client.defaults.baseURL).toEqual('wallet.service.url');
 });
