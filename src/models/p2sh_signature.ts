0fe8197ec9dd33bc9024849d4edeef156bc62b8d
André Carneiro <andreluizmrcarneiro@gmail.com>
2022-03-18 18:35:21 -0300

    chore: docstring

--- src/models/p2sh_signature.ts
+++ src/models/p2sh_signature.ts
@@ -8,6 +8,18 @@
 import { util } from 'bitcore-lib';
 
 
+/**
+ * This class purpose is serialization/deserialization of signatures from a MultiSig participant
+ * The structure of the serialized signature string is:
+ * "<pubkey>|<index>:<signature>|<index>:<signature>|..."
+ *
+ * The `pubkey` is required so we can identify the original signer (and his position on the redeemScript)
+ * The `<index>:<signature>` pair is the input index and the signature for that input.
+ * The signature is formatted to DER and hex encoded.
+ *
+ * With this information we will be able to encode the signatures for all inputs on one string.
+ * It also has all information needed to assemble the input data if you have enough participants' P2SHSignature serialized signatures.
+ */
 class P2SHSignature {
   pubkey: String;
   signatures: Record<number, String>;
