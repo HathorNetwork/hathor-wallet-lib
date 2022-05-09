935b80b2beded09dd285bb67c7416efbc49eadc2
André Abadesso <andre.abadesso@gmail.com>
2022-05-09 11:02:43 -0300

    refactor: network now extends the default Network model to call config.setNetwork

--- src/network.js
+++ src/network.js
@@ -5,9 +5,36 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import config from './config';
 import Network from './models/network';
 
+/**
+ * Extend the network to be able to set config when setNetwork is called on the singleton
+ */
+class ExtendedNetwork extends Network {
+  constructor(name) {
+    super(name);
+  }
+
+  /* Until we replace the network singleton with the new config singleton, we need to
+   * maintain reverse compatibility as there are multiple use cases using the lib with
+   * this network singleton
+   *
+   * Since config.setNetwork also calls networkInstance.setNetwork, we need the skipConfig
+   * parameter to avoid a cyclic call
+   *
+   * TODO: Remove this when the network singleton is completely deprecated
+   */
+  setNetwork(name, skipConfig = false) {
+    super.setNetwork(name);
+ 
+    if (!skipConfig) {
+      config.setNetwork(name);
+    }
+  }
+}
+
 // Default network for the lib is testnet
-const instance = new Network('testnet');
+const instance = new ExtendedNetwork('testnet');
 
 export default instance;
