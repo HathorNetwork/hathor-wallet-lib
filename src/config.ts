09d82c705345873670e746f673224499f96485c9
André Abadesso <andre.abadesso@gmail.com>
2022-05-09 11:03:18 -0300

    refactor: setting network on the network singleton temporarily

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ import { GetWalletServiceUrlError, GetWalletServiceWsUrlError } from './errors';
 
 // Default server and network user will connect when none have been chosen
 export const DEFAULT_SERVER = 'https://node1.mainnet.hathor.network/v1a/';
-const DEFAULT_NETWORK = 'mainnet';
+const DEFAULT_NETWORK = new Network('mainnet');
 
 // Base URL for the tx mining api
 const TX_MINING_MAINNET_URL = 'https://txmining.mainnet.hathor.network/';
@@ -17,85 +17,85 @@ const EXPLORER_SERVICE_MAINNET_BASE_URL  = 'https://explorer-service.hathor.netw
 const EXPLORER_SERVICE_TESTNET_BASE_URL  = 'https://explorer-service.testnet.hathor.network/';
 
 class Config {
-    TX_MINING_URL?: string;
-    TX_MINING_API_KEY?: string;
-    WALLET_SERVICE_BASE_URL?: string;
-    WALLET_SERVICE_BASE_WS_URL?: string;
-    EXPLORER_SERVICE_BASE_URL?: string;
-    SERVER_URL?: string;
-    NETWORK?: string;
+  TX_MINING_URL?: string;
+  TX_MINING_API_KEY?: string;
+  WALLET_SERVICE_BASE_URL?: string;
+  WALLET_SERVICE_BASE_WS_URL?: string;
+  EXPLORER_SERVICE_BASE_URL?: string;
+  SERVER_URL?: string;
+  NETWORK?: string;
 
-    /**
-    * Sets the tx mining service url that will be returned by the config object.
-    *
-    * @param {string} url - The url to be set
-    */
-    setTxMiningUrl(url: string) {
-        this.TX_MINING_URL = url;
+  /**
+   * Sets the tx mining service url that will be returned by the config object.
+   *
+   * @param {string} url - The url to be set
+   */
+  setTxMiningUrl(url: string) {
+    this.TX_MINING_URL = url;
+  }
+
+  /**
+   * Returns the correct base url constant for tx mining.
+   * If the url was explicitly set using the config object, it is always returned.
+   * Otherwise, we return it based on the network set with networkInstance.
+   *
+   * @return The tx mining service url
+   */
+  getTxMiningUrl(): string {
+    if (this.TX_MINING_URL) {
+      return this.TX_MINING_URL;
     }
 
-    /**
-     * Returns the correct base url constant for tx mining.
-     * If the url was explicitly set using the config object, it is always returned.
-     * Otherwise, we return it based on the network set with networkInstance.
-     *
-     * @return {string} The tx mining service url
-     */
-    getTxMiningUrl() {
-        if (this.TX_MINING_URL) {
-            return this.TX_MINING_URL;
-        }
+  // Keeps the old behavior for cases that don't explicitly set a TX_MINING_URL
+  if (networkInstance.name == 'mainnet') {
+    return TX_MINING_MAINNET_URL;
+  } else if (networkInstance.name == 'testnet') {
+    return TX_MINING_TESTNET_URL;
+  } else {
+    throw new Error(`Network ${networkInstance.name} doesn't have a correspondent tx mining service url. You should set it explicitly.`);
+  }
+}
 
-        // Keeps the old behavior for cases that don't explicitly set a TX_MINING_URL
-        if (networkInstance.name == 'mainnet') {
-            return TX_MINING_MAINNET_URL;
-        } else if (networkInstance.name == 'testnet') {
-            return TX_MINING_TESTNET_URL;
-        } else {
-            throw new Error(`Network ${networkInstance.name} doesn't have a correspondent tx mining service url. You should set it explicitly.`);
-        }
-    }
+/**
+ * Sets the tx mining service api key that will be returned by the config object.
+ *
+ * @param apiKey - The api key to be set
+ */
+setTxMiningApiKey(apiKey: string) {
+  this.TX_MINING_API_KEY = apiKey;
+}
 
-    /**
-    * Sets the tx mining service api key that will be returned by the config object.
-    *
-    * @param {string} apiKey - The api key to be set
-    */
-    setTxMiningApiKey(apiKey: string) {
-        this.TX_MINING_API_KEY = apiKey;
-    }
+/**
+ * Gets the configured api key for tx-mining-service
+ * 
+ * @returns {string} The api key
+ */
+getTxMiningApiKey() {
+  return this.TX_MINING_API_KEY;
+}
 
-    /**
-     * Gets the configured api key for tx-mining-service
-     * 
-     * @returns {string} The api key
-     */
-    getTxMiningApiKey() {
-        return this.TX_MINING_API_KEY;
-    }
+/**
+ * Sets the wallet service url that will be returned by the config object.
+ *
+ * @param {string} url - The url to be set
+ */
+setWalletServiceBaseUrl(url: string): void {
+  this.WALLET_SERVICE_BASE_URL = url;
+}
 
-    /**
-    * Sets the wallet service url that will be returned by the config object.
-    *
-    * @param {string} url - The url to be set
-    */
-    setWalletServiceBaseUrl(url: string): void {
-      this.WALLET_SERVICE_BASE_URL = url;
-    }
+/**
+ * Returns the base url for wallet service set previously using setWalletServiceBaseUrl
+ *
+ * Throws an error if it is not yet set.
+ *
+ * @return {string} The wallet service url
+ */
+getWalletServiceBaseUrl(): string {
+  if (!this.WALLET_SERVICE_BASE_URL) {
+    throw new GetWalletServiceUrlError('Wallet service base URL not set.');
+  }
 
-    /**
-     * Returns the base url for wallet service set previously using setWalletServiceBaseUrl
-     *
-     * Throws an error if it is not yet set.
-     *
-     * @return {string} The wallet service url
-     */
-    getWalletServiceBaseUrl(): string {
-      if (!this.WALLET_SERVICE_BASE_URL) {
-        throw new GetWalletServiceUrlError('Wallet service base URL not set.');
-      }
-
-      return this.WALLET_SERVICE_BASE_URL;
+return this.WALLET_SERVICE_BASE_URL;
     }
 
     /**
@@ -114,59 +114,59 @@ class Config {
     }
 
     /**
-    * Sets the wallet service websocket url that will be returned by the config object.
-    *
-    * @param {string} url - The url to be set
-    */
+     * Sets the wallet service websocket url that will be returned by the config object.
+     *
+     * @param {string} url - The url to be set
+     */
     setWalletServiceBaseWsUrl(url: string): void {
       this.WALLET_SERVICE_BASE_WS_URL = url;
     }
 
     /**
-    * Sets the explorer service url that will be returned by the config object.
-    *
-    * @param {string} url - The url to be set
-    */
+     * Sets the explorer service url that will be returned by the config object.
+     *
+     * @param {string} url - The url to be set
+     */
     setExplorerServiceBaseUrl(url: string) {
-        this.EXPLORER_SERVICE_BASE_URL = url;
+      this.EXPLORER_SERVICE_BASE_URL = url;
     }
 
     /**
-    * Returns the correct base url constant for explorer service.
-    * If the url was explicitly set using the config object, it is always returned.
-    * Otherwise, we return it based on the provided network object.
-    *
-    * If the url was not set in the config, and no network is provided, we throw an Error.
-    *
-    * @param {string} network - The name of the network to be used to select the url.
-    * @return {string} The explorer service url
-    */
+     * Returns the correct base url constant for explorer service.
+     * If the url was explicitly set using the config object, it is always returned.
+     * Otherwise, we return it based on the provided network object.
+     *
+     * If the url was not set in the config, and no network is provided, we throw an Error.
+     *
+     * @param {string} network - The name of the network to be used to select the url.
+     * @return {string} The explorer service url
+     */
     getExplorerServiceBaseUrl(network: string) {
-        if (this.EXPLORER_SERVICE_BASE_URL) {
-            return this.EXPLORER_SERVICE_BASE_URL;
-        }
+      if (this.EXPLORER_SERVICE_BASE_URL) {
+        return this.EXPLORER_SERVICE_BASE_URL;
+      }
 
-        if (!network) {
-            throw new Error('You should either provide a network or call setExplorerServiceBaseUrl before calling this.');
-        }
+      if (!network) {
+        throw new Error('You should either provide a network or call setExplorerServiceBaseUrl before calling this.');
+      }
 
-        // Keeps the old behavior for cases that don't explicitly set a EXPLORER_SERVICE_BASE_URL
-        if (network == 'mainnet') {
-            return EXPLORER_SERVICE_MAINNET_BASE_URL;
-        } else if (network == 'testnet'){
-            return EXPLORER_SERVICE_TESTNET_BASE_URL;
-        } else {
-            throw new Error(`Network ${network} doesn't have a correspondent explorer service url. You should set it explicitly.`);
-        }
+      // Keeps the old behavior for cases that don't explicitly set a EXPLORER_SERVICE_BASE_URL
+      if (network == 'mainnet') {
+        return EXPLORER_SERVICE_MAINNET_BASE_URL;
+      } else if (network == 'testnet'){
+        return EXPLORER_SERVICE_TESTNET_BASE_URL;
+      } else {
+        throw new Error(`Network ${network} doesn't have a correspondent explorer service url. You should set it explicitly.`);
+      }
     }
 
     /**
-    * Sets the fullnode server url that will be returned by the config object.
-    *
-    * @param {string} url - The url to be set
-    */
+     * Sets the fullnode server url that will be returned by the config object.
+     *
+     * @param {string} url - The url to be set
+     */
     setServerUrl(url: string) {
-        this.SERVER_URL = url;
+      this.SERVER_URL = url;
     }
 
     /**
@@ -180,39 +180,47 @@ class Config {
      * @return {string} Server URL according to the priority described above
      */
     getServerUrl() {
-        if (this.SERVER_URL) {
-            return this.SERVER_URL;
+      if (this.SERVER_URL) {
+        return this.SERVER_URL;
+      }
+
+      if (storage.isInitialized()) {
+        const server = storage.getItem('wallet:server');
+        const defaultServer = storage.getItem('wallet:defaultServer');
+
+        if (server !== null) {
+          return server;
+        } else if (defaultServer !== null) {
+          return defaultServer
         }
+      }
 
-        if (storage.isInitialized()) {
-            const server = storage.getItem('wallet:server');
-            const defaultServer = storage.getItem('wallet:defaultServer');
-
-            if (server !== null) {
-                return server;
-            } else if (defaultServer !== null) {
-                return defaultServer
-            }
-        }
-
-        return DEFAULT_SERVER;
+      return DEFAULT_SERVER;
     }
 
     /**
      * Sets the current network the wallet is connected to
      */
     setNetwork(network: string) {
+      /* Until we replace the network singleton with the new config singleton, we need to
+       * maintain reverse compatibility as there are multiple use cases using the lib with
+       * the network singleton
+       *
+       * TODO: Remove this when the network singleton is completely deprecated
+       */
+      networkInstance.setNetwork(network, true);
+
       this.NETWORK = network;
     }
 
     /**
-    * Gets the current network
-    *
-    * There is more than one method of setting this.
-    * The priority will be given to the network set using the config object.
-    * If not set, we look next into the storage object keys.
-    * If still not set, the default url is returned
-    */
+     * Gets the current network
+     *
+     * There is more than one method of setting this.
+     * The priority will be given to the network set using the config object.
+     * If not set, we look next into the storage object keys.
+     * If still not set, the default url is returned
+     */
     getNetwork() {
       if (this.NETWORK) {
         return this.NETWORK;
@@ -222,7 +230,7 @@ class Config {
         const network = storage.getItem('wallet:network');
 
         if (network !== null) {
-          return network;
+          return new Network(network);
         }
       }
 
