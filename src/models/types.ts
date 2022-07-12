2ae7660ab6d5ffdc44ab4e36c4e1725868e4c8ac
tuliomir <tulio.mir@gmail.com>
2022-07-12 20:13:17 -0300

    chore: adds missing file header

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,4 +1,11 @@
 /**
+ * Copyright (c) Hathor Labs and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
  * An output object enriched by the wallet's history methods
  */
 export type HistoryTransactionOutput = {
