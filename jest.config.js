468a10541881ce921cd9359e3da9896bd3fd84ca
tuliomir <tulio.mir@gmail.com>
2022-08-12 16:19:04 -0300

    chore: adds coverage config on jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+  coverageDirectory: 'coverage',
+  collectCoverage: true,
   collectCoverageFrom: ["<rootDir>/src/**/*.js","<rootDir>/src/**/*.ts","!<rootDir>/node_modules/"],
+  coverageReporters: ['text-summary', 'lcov', 'clover'],
   modulePathIgnorePatterns: ["__fixtures__/*","integration/*"],
   coverageThreshold: {
     global: {
@@ -10,5 +13,4 @@ module.exports = {
       statements: 85
     }
   },
-  coverageReporters: ["html","text"]
 };
