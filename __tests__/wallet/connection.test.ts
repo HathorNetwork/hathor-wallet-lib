60558c2ee90da8326e6b4974b246a4690b237eeb
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2023-01-06 14:30:12 +0000

    chore: update dummy connection test

--- __tests__/wallet/connection.test.ts
+++ __tests__/wallet/connection.test.ts
@@ -1,36 +1,64 @@
 import { DummyWalletServiceConnection } from "../../src/wallet/connection";
+import { ConnectionState } from "../../src/wallet/types";
+
+class DummyConnectionUnderTest extends DummyWalletServiceConnection {
+  constructor() {
+    super();
+  }
+
+  public getState(): ConnectionState {
+    return this.state;
+  }
+}
 
 describe('DummyWalletServiceConnection', () => {
 
-  it('should throw error when call start and walletId is not set', () => {
-    const connection = new DummyWalletServiceConnection({ walletId: undefined });
-    expect(() => connection.start()).toThrowError('Wallet id should be set before connection start.');
+  it('should do nothing on start call', () => {
+    const connection = new DummyConnectionUnderTest({ walletId: undefined });
+    expect(() => connection.start()).not.toThrow();
   });
 
-  it('should stop connection even if was not started', () => {
-    const connection = new DummyWalletServiceConnection();
+  it('should do nothing on stop call', () => {
+    const connection = new DummyConnectionUnderTest();
     expect(() => connection.stop()).not.toThrow();
-    expect(connection.isStateClosed()).toBeTruthy();
   });
 
-  it('should change state on onConnectionChange', () => {
-    const connection = new DummyWalletServiceConnection();
-    expect(() => connection.onConnectionChange(true)).not.toThrow();
-    expect(connection.isStateConnected()).toBeTruthy();
+  it('should do nothing on endConnection call', () => {
+    const connection = new DummyConnectionUnderTest();
+    expect(() => connection.endConnection()).not.toThrow();
+  });
 
-    expect(() => connection.onConnectionChange(false)).not.toThrow();
-    expect(connection.isStateConnecting()).toBeTruthy();
+  it('should do nothing on setup call', () => {
+    const connection = new DummyConnectionUnderTest();
+    expect(() => connection.setup()).not.toThrow();
+  });
+
+  it('should do nothing on handleWalletMessage call', () => {
+    const connection = new DummyConnectionUnderTest();
+    expect(() => connection.handleWalletMessage(null)).not.toThrow();
+  });
+
+  it('should do nothing on onConnectionChange call', () => {
+    const connection = new DummyConnectionUnderTest();
+    expect(() => connection.onConnectionChange(true)).not.toThrow();
+  });
+
+  it('should change state when setState is called', () => {
+    const connection = new DummyConnectionUnderTest();
+    expect(() => connection.setState(ConnectionState.CONNECTED)).not.toThrow();
+
+    expect(connection.getState()).toStrictEqual(ConnectionState.CONNECTED);
   });
 
   it('should return current server and network', () => {
-    const connection = new DummyWalletServiceConnection({ network: 'testnet', walletId: 'walletId' });
+    const connection = new DummyConnectionUnderTest({ network: 'testnet', walletId: 'walletId' });
     expect(connection.getCurrentServer()).toEqual('http://localhost:8080/');
     expect(connection.getCurrentNetwork()).toEqual('testnet');
   });
 
   // test setWalletId
   it('should set walletId', () => {
-    const connection = new DummyWalletServiceConnection();
+    const connection = new DummyConnectionUnderTest();
     expect(connection.walletId).toEqual('dummy-wallet');
 
     expect(() => connection.setWalletId('walletId')).not.toThrow();
