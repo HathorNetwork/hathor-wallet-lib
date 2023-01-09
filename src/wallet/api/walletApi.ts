21bbd25bd5849cf86c25575e95b3ac0299d80b23
André Abadesso <andre.abadesso@gmail.com>
2023-01-09 13:46:16 -0300

    refactor: handling error in graphviz query in the wallet-service facade

--- src/wallet/api/walletApi.ts
+++ src/wallet/api/walletApi.ts
@@ -276,10 +276,21 @@ const walletApi = {
     const axios = await axiosInstance(wallet, true);
     const response = await axios.get(`wallet/proxy/graphviz/neighbours?txId=${txId}&graphType=${graphType}&maxLevel=${maxLevel}`);
     if (response.status === 200) {
+      // The service might answer a status code 200 but output an error message like
+      // { success: false, message: '...' }, we need to handle it.
+      //
+      // We also need to check if `success` is a key to the object since this API will return
+      // a string on success.
+      if (Object.hasOwnProperty.call(response.data, 'success') && !response.data.success) {
+        throw new WalletRequestError(`Error getting neighbors data for ${txId} from the proxied fullnode.`, {
+          cause: response.data.message,
+        });
+      }
+
       return response.data;
     }
 
-    throw new WalletRequestError('Error getting transaction confirmation data by its id from the proxied fullnode.', {
+    throw new WalletRequestError(`Error getting neighbors data for ${txId} from the proxied fullnode.`, {
       cause: response.data,
     });
   },
