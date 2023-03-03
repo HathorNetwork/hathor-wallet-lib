69a38eec2a0558f64681da544e0222d47371d443
Tulio Miranda <tulio.mir@gmail.com>
2023-03-03 18:14:52 -0300

    feat: Swap Service `get` (#468)
    * feat: get endpoint
    
    * feat: check against invalid decoded partialTx
    
    * test: ensures history decrypting
    
    * test: increase coverage and param checking
    
    * docs: adds throws declaration
    
    * docs: fix incorrect jsdocs example

--- __tests__/wallet/api/swapService.test.ts
+++ __tests__/wallet/api/swapService.test.ts
@@ -1,8 +1,9 @@
 
-import { decryptString, encryptString, hashPassword, create } from '../../../src/wallet/api/swapService'
+import { decryptString, encryptString, hashPassword, create, get } from '../../../src/wallet/api/swapService'
 import config from '../../../src/config';
 import MockAdapter from 'axios-mock-adapter';
 import axios from 'axios';
+import AES from 'crypto-js/aes';
 
 const mockAxiosAdapter = new MockAdapter(axios);
 
@@ -12,6 +13,20 @@ describe('hashing and encrypting', () => {
     const hashedPassword = hashPassword(password);
 
     expect(hashedPassword).toHaveLength(64);
+  })
+
+  it('should reject calls with missing parameters', () => {
+    const originalString = 'PartialTx|123123||';
+
+    // @ts-ignore
+    expect(() => encryptString()).toThrowError('Missing encrypted string');
+    // @ts-ignore
+    expect(() => encryptString(originalString)).toThrowError('Missing password');
+
+    // @ts-ignore
+    expect(() => decryptString()).toThrowError('Missing encrypted string');
+    // @ts-ignore
+    expect(() => decryptString(originalString)).toThrowError('Missing password');
   })
 
   it('should correctly encrypt and decrypt a string', () => {
@@ -82,5 +97,167 @@ describe('create api', () => {
     mockAxiosAdapter.onPost('/').reply(200, responseData)
     await expect(create('PartialTx|0001000000000000000000000063f78c0e0000000000||', 'abc'))
       .resolves.toStrictEqual(responseData)
+  })
+})
+
+describe('get api', () => {
+
+  it('should throw missing parameter errors', async () => {
+    // @ts-ignore
+    await expect(get()).rejects.toThrowError('Missing proposalId');
+    // @ts-ignore
+    await expect(get('b4a5b077-c599-41e8-a791-85e08efcb1da'))
+      .rejects.toThrowError('Missing password');
+  })
+
+  it('should handle backend errors', async () => {
+    config.setSwapServiceBaseUrl('http://mock-swap-url/')
+
+    mockAxiosAdapter.onGet('/b4a5b077-c599-41e8-a791-85e08efcb1da').reply(503)
+    await expect(get('b4a5b077-c599-41e8-a791-85e08efcb1da', 'abc'))
+      .rejects.toThrowError('Request failed with status code 503');
+  })
+
+  it('should throw if the decoded string is corrupted', async () => {
+    const originalPartialTx = 'PartialTx|0001000000000000000000000063f78c0e0000000000||';
+    const password = 'abc';
+    const incorrectPassword = 'bcd';
+
+    config.setSwapServiceBaseUrl('http://mock-swap-url/')
+    const rawHttpBody = {
+      id: 'b4a5b077-c599-41e8-a791-85e08efcb1da',
+      partialTx: encryptString(originalPartialTx, password),
+      signatures: null,
+      timestamp: 'Wed Mar 01 2023 18:56:00 GMT-0300 (Brasilia Standard Time)',
+      version: 0,
+      history: []
+    }
+
+    mockAxiosAdapter.onGet('/b4a5b077-c599-41e8-a791-85e08efcb1da').reply(200, rawHttpBody)
+    const decryptMock = jest.spyOn(AES, 'decrypt').mockImplementationOnce(() => {
+      throw new Error('Malformed UTF-8 data');
+    });
+    await expect(get('b4a5b077-c599-41e8-a791-85e08efcb1da', incorrectPassword))
+      .rejects.toThrowError('Incorrect password: could not decode the proposal');
+    decryptMock.mockRestore();
+  })
+
+  it('should throw if the decrypted string is parseable, but the password cannot decode it', async () => {
+    const originalPartialTx = 'PartialTx|0001000000000000000000000063f78c0e0000000000||';
+    const password = 'abc';
+    const incorrectPassword = 'bcd';
+
+    config.setSwapServiceBaseUrl('http://mock-swap-url/')
+    const rawHttpBody = {
+      id: 'b4a5b077-c599-41e8-a791-85e08efcb1da',
+      partialTx: encryptString(originalPartialTx, password),
+      signatures: null,
+      timestamp: 'Wed Mar 01 2023 18:56:00 GMT-0300 (Brasilia Standard Time)',
+      version: 0,
+      history: []
+    }
+
+    mockAxiosAdapter.onGet('/b4a5b077-c599-41e8-a791-85e08efcb1da').reply(200, rawHttpBody)
+    const decryptMock = jest.spyOn(AES, 'decrypt').mockImplementationOnce(() => 'invalid string');
+    await expect(get('b4a5b077-c599-41e8-a791-85e08efcb1da', incorrectPassword))
+      .rejects.toThrowError('Incorrect password: could not decode the proposal');
+    decryptMock.mockRestore();
+  })
+
+  it('should return the backend results on a successful request', async () => {
+    const originalPartialTx = 'PartialTx|0001000000000000000000000063f78c0e0000000000||';
+    const password = 'abc';
+
+    config.setSwapServiceBaseUrl('http://mock-swap-url/')
+    const rawHttpBody = {
+      id: 'b4a5b077-c599-41e8-a791-85e08efcb1da',
+      partialTx: encryptString(originalPartialTx, password),
+      signatures: null,
+      timestamp: 'Wed Mar 01 2023 18:56:00 GMT-0300 (Brasilia Standard Time)',
+      version: 0,
+      history: []
+    }
+
+    const responseData = {
+      proposalId: 'b4a5b077-c599-41e8-a791-85e08efcb1da',
+      partialTx: originalPartialTx,
+      signatures: null,
+      timestamp: 'Wed Mar 01 2023 18:56:00 GMT-0300 (Brasilia Standard Time)',
+      version: 0,
+      history: []
+    };
+    mockAxiosAdapter.onGet('/b4a5b077-c599-41e8-a791-85e08efcb1da').reply(200, rawHttpBody)
+    const resolvedData = await get('b4a5b077-c599-41e8-a791-85e08efcb1da', password);
+    expect(resolvedData).toStrictEqual(responseData)
+  })
+
+  it('should correctly parse the history', async () => {
+    const originalPartialTx = 'PartialTx|0001000000000000000000000063f78c0e0000000000||';
+    const password = 'abc';
+    const encryptedPartialTx = encryptString(originalPartialTx, password);
+
+    config.setSwapServiceBaseUrl('http://mock-swap-url/')
+    const rawHttpBody = {
+      id: 'b4a5b077-c599-41e8-a791-85e08efcb1da',
+      partialTx: encryptedPartialTx,
+      signatures: null,
+      timestamp: 'Wed Mar 01 2023 18:56:00 GMT-0300 (Brasilia Standard Time)',
+      version: 0,
+      history: [{
+        partialTx: encryptedPartialTx,
+        timestamp: 'Wed Fev 01 2023 18:56:00 GMT-0300 (Brasilia Standard Time)'
+      }]
+    }
+
+    const responseData = {
+      proposalId: 'b4a5b077-c599-41e8-a791-85e08efcb1da',
+      partialTx: originalPartialTx,
+      signatures: null,
+      timestamp: 'Wed Mar 01 2023 18:56:00 GMT-0300 (Brasilia Standard Time)',
+      version: 0,
+      history: [{
+        partialTx: originalPartialTx,
+        timestamp: 'Wed Fev 01 2023 18:56:00 GMT-0300 (Brasilia Standard Time)'
+      }]
+    };
+    mockAxiosAdapter.onGet('/b4a5b077-c599-41e8-a791-85e08efcb1da').reply(200, rawHttpBody)
+    const resolvedData = await get('b4a5b077-c599-41e8-a791-85e08efcb1da', password);
+    expect(resolvedData).toStrictEqual(responseData)
+  })
+
+  it('should correctly decode the signatures', async () => {
+    const originalPartialTx = 'PartialTx|0001000000000000000000000063f78c0e0000000000||';
+    const originalSignatures = 'PartialTxInputData|0001010204002f91917e63ce0f9d21a6b50adc45539f0ffe1d35b|0:4630440220514e0867c310232eb9ab1c18274a10b5bf163b8cd681';
+    const password = 'abc';
+    const encryptedPartialTx = encryptString(originalPartialTx, password);
+    const encryptedSignatures = encryptString(originalSignatures, password);
+
+    config.setSwapServiceBaseUrl('http://mock-swap-url/')
+    const rawHttpBody = {
+      id: 'b4a5b077-c599-41e8-a791-85e08efcb1da',
+      partialTx: encryptedPartialTx,
+      signatures: encryptedSignatures,
+      timestamp: 'Wed Mar 01 2023 18:56:00 GMT-0300 (Brasilia Standard Time)',
+      version: 0,
+      history: [{
+        partialTx: encryptedPartialTx,
+        timestamp: 'Wed Fev 01 2023 18:56:00 GMT-0300 (Brasilia Standard Time)'
+      }]
+    }
+
+    const responseData = {
+      proposalId: 'b4a5b077-c599-41e8-a791-85e08efcb1da',
+      partialTx: originalPartialTx,
+      signatures: originalSignatures,
+      timestamp: 'Wed Mar 01 2023 18:56:00 GMT-0300 (Brasilia Standard Time)',
+      version: 0,
+      history: [{
+        partialTx: originalPartialTx,
+        timestamp: 'Wed Fev 01 2023 18:56:00 GMT-0300 (Brasilia Standard Time)'
+      }]
+    };
+    mockAxiosAdapter.onGet('/b4a5b077-c599-41e8-a791-85e08efcb1da').reply(200, rawHttpBody)
+    const resolvedData = await get('b4a5b077-c599-41e8-a791-85e08efcb1da', password);
+    expect(resolvedData).toStrictEqual(responseData)
   })
 })
--- src/wallet/api/swapService.ts
+++ src/wallet/api/swapService.ts
@@ -6,11 +6,26 @@
  */
 
 import config from "../../config";
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { TIMEOUT, } from '../../constants';
 import sha256 from 'crypto-js/sha256';
 import AES from 'crypto-js/aes';
 import CryptoJS from 'crypto-js';
+import { AtomicSwapProposal } from "../../models/types";
+import { PartialTxPrefix } from '../../models/partial_tx';
+
+/**
+ * This interface represents the type returned on the HTTP response, with its untreated and encrypted data.
+ * The results should be translated to an `AtomicSwapProposal` interface before being sent out of this service.
+ */
+interface RawBackendProposal {
+  id: string,
+  partialTx: string,
+  signatures: string | null,
+  timestamp: string,
+  version: number,
+  history: { partialTx: string, timestamp: string }[]
+}
 
 /**
  * Encrypts a string ( PartialTx or Signatures ) before sending it to the backend
@@ -18,6 +33,12 @@ import CryptoJS from 'crypto-js';
  * @param password
  */
 export function encryptString(serialized: string, password: string): string {
+  if (!serialized) {
+    throw new Error('Missing encrypted string');
+  }
+  if (!password) {
+    throw new Error('Missing password');
+  }
   const aesEncryptedObject = AES.encrypt(serialized, password);
 
   // Serializing the cipher output in Base64 to avoid loss of encryption parameter data
@@ -31,6 +52,12 @@ export function encryptString(serialized: string, password: string): string {
  * @param password
  */
 export function decryptString(serialized: string, password: string): string {
+  if (!serialized) {
+    throw new Error('Missing encrypted string');
+  }
+  if (!password) {
+    throw new Error('Missing password');
+  }
   const aesDecryptedObject = AES.decrypt(serialized, password);
   return aesDecryptedObject.toString(CryptoJS.enc.Utf8);
 }
@@ -87,4 +114,61 @@ export const create = async (serializedPartialTx: string, password: string) => {
 
   const { data } = await swapAxios.post<{ success: boolean, id: string }>('/', payload);
   return data;
+};
+
+/**
+ * Fetches from the Atomic Swap Service the most up-to-date version of the proposal by the given id
+ * and decrypts it locally
+ * @throws {Error} When the swap service network is not configured
+ * @throws {Error} When the password is incorrect and the proposal cannot be decoded
+ * @param proposalId
+ * @param password
+ * @example
+ * const results = await get('b4a5b077-c599-41e8-a791-85e08efcb1da', 'pass123')
+ */
+export const get = async (proposalId: string, password: string): Promise<AtomicSwapProposal> => {
+  if (!proposalId) {
+    throw new Error('Missing proposalId');
+  }
+  if (!password) {
+    throw new Error('Missing password');
+  }
+
+  const swapAxios = await axiosInstance();
+  const options = {
+    headers: { 'X-Auth-Password': hashPassword(password) }
+  } as AxiosRequestConfig;
+
+  const { data } = await swapAxios.get<RawBackendProposal>(`/${proposalId}`, options);
+
+  // Decrypting the backend contents and handling its possible failures
+  try {
+    const decryptedData: AtomicSwapProposal = {
+      proposalId: data.id,
+      version: data.version,
+      timestamp: data.timestamp,
+      partialTx: decryptString(data.partialTx, password),
+      signatures: data.signatures ? decryptString(data.signatures, password) : null,
+      history: data.history.map(r => ({
+        partialTx: decryptString(r.partialTx, password),
+        timestamp: r.timestamp,
+      }))
+    };
+
+    // If the PartialTx does not have the correct prefix, it was not correctly decoded: incorrect password
+    if (!decryptedData.partialTx.startsWith(PartialTxPrefix)) {
+      throw new Error('Incorrect password: could not decode the proposal');
+    }
+
+    // Decoding was successful, return the data
+    return decryptedData;
+  } catch (err) {
+    // If the failure was specifically on the decoding, our password was incorrect.
+    if (err.message === "Malformed UTF-8 data") {
+      throw new Error('Incorrect password: could not decode the proposal');
+    }
+
+    // Rethrow any other errors that may happen
+    throw err;
+  }
 };
