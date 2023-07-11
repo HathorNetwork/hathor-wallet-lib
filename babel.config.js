7a6f181301134bbcbd501c8997a97004be3a3dfa
André Carneiro <andreluizmrcarneiro@gmail.com>
2023-07-11 20:23:57 -0300

    chore: default preset-env for jest

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ module.exports = {
   presets: [
     '@babel/preset-react',
     '@babel/preset-typescript',
+    '@babel/preset-env',
   ],
   plugins: [
     "@babel/plugin-transform-async-generator-functions",
