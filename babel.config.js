0c469b107d1bf4b7152a6a9f57bc1ffc632a305a
André Carneiro <andreluizmrcarneiro@gmail.com>
2023-07-11 20:14:25 -0300

    feat: compile index file

--- babel.config.js
+++ babel.config.js
@@ -2,15 +2,41 @@
 module.exports = {
   presets: [
     '@babel/preset-react',
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
     '@babel/preset-typescript',
   ],
-  plugins: ["@babel/plugin-transform-async-generator-functions"],
+  plugins: [
+    "@babel/plugin-transform-async-generator-functions",
+  ],
+  env: {
+    node: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: 'current',
+            },
+          },
+        ],
+      ],
+    },
+    browser: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              browsers: [
+                "Chrome >= 60",
+                "Safari >= 10.1",
+                "iOS >= 10.3",
+                "Firefox >= 54",
+                "Edge >= 15"
+              ]
+            },
+          },
+        ],
+      ],
+    },
+  }
 };
