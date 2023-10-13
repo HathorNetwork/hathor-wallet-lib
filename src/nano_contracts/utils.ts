266ce51fe99f0514f7e29d0589ff418ba248ef29
Pedro Ferreira <pedro@hathor.network>
2023-10-13 05:36:46 -0300

    chore: remove unused import

--- src/nano_contracts/utils.ts
+++ src/nano_contracts/utils.ts
@@ -11,7 +11,6 @@ import SendTransaction from '../new/sendTransaction';
 import HathorWallet from '../new/wallet';
 import NanoContract from './nano_contract';
 import Transaction from '../models/transaction';
-import Deserializer from './deserializer';
 import Network from '../models/network';
 import ScriptData from '../models/script_data';
 import { hexToBuffer, bufferToHex, unpackLen } from '../utils/buffer';
