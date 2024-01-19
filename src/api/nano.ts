a47a11766a4f10d3225fad1dea8168fb42ab886b
Pedro Ferreira <pedro@hathor.network>
2024-01-19 11:02:06 -0300

    feat: add nano apis and error for requests

---
+++ src/api/nano.ts
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) Hathor Labs and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { createRequestInstance } from './axiosInstance';
+import { NanoRequestError } from '../errors';
+
+/**
+ * Api calls for nano contracts
+ *
+ * @namespace ApiNanoContracts
+ */
+
+const ncApi = {
+  /**
+   * Call get nano contracts state API
+   *
+   * @param id Nano Contract ID
+   * @param fields Array of fields to get state
+   *
+   * @return {Promise}
+   * @memberof ApiNanoContracts
+   * @inner
+   */
+  async getNanoContractState(id: string, fields: string[], balances: string[], calls: string[]) {
+    const data = { id, fields, balances, calls };
+    const axios = await createRequestInstance();
+    const response = await axios.get(`nano_contract/state`, {params: data});
+    const responseData = response.data;
+    if (response.status === 200 && responseData.success) {
+      return responseData;
+    } else {
+      throw new NanoRequestError('Error getting nano contract state.')
+    }
+  },
+
+  /**
+   * Call get nano contracts history API
+   *
+   * @param id Nano Contract ID
+   *
+   * @return {Promise}
+   * @memberof ApiNanoContracts
+   * @inner
+   */
+  async getNanoContractHistory(id: string, count: number | null = null, after: string | null = null) {
+    const data = { id };
+    if (count) {
+      data['count'] = count;
+    }
+
+    if (after) {
+      data['after'] = after;
+    }
+    const axios = await createRequestInstance();
+    const response = await axios.get(`nano_contract/history`, {params: data});
+    const responseData = response.data;
+    if (response.status === 200 && responseData.success) {
+      return responseData;
+    } else {
+      throw new NanoRequestError('Error getting nano contract history.')
+    }
+  },
+
+  /**
+   * Call get blueprint information
+   *
+   * @param id Blueprint ID
+   *
+   * @return {Promise}
+   * @memberof ApiNanoContracts
+   * @inner
+   */
+  async getBlueprintInformation(id: string) {
+    const data = { blueprint_id: id };
+    const axios = await createRequestInstance();
+    const response = await axios.get(`nano_contract/blueprint`, { params: data });
+    const responseData = response.data;
+    if (response.status === 200) {
+      return responseData;
+    } else {
+      throw new NanoRequestError('Error getting blueprint information.')
+    }
+  },
+};
+
+export default ncApi;
