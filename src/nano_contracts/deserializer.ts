07e7cbeb89d0f6af1b8d70330340513a8264663a
Pedro Ferreira <pedro@hathor.network>
2024-05-10 15:11:00 -0300

    feat: add support for optional parameters in nano contract serializer/deserializer (#647)

--- src/nano_contracts/deserializer.ts
+++ src/nano_contracts/deserializer.ts
@@ -22,6 +22,12 @@ class Deserializer {
    * @inner
    */
   deserializeFromType(value: Buffer, type: string): any {
+    if (type.endsWith('?')) {
+      // It's an optional
+      const optionalType = type.slice(0, -1);
+      return this.toOptional(value, optionalType);
+    }
+
     if (type.startsWith('SignedData[')) {
       return this.toSigned(value, type);
     }
@@ -107,6 +113,30 @@ class Deserializer {
   }
 
   /**
+   * Deserialize an optional value
+   *
+   * First we check the first byte. If it's 0, then we return null.
+   *
+   * Otherwise, we deserialize the rest of the buffer to the type.
+   *
+   * @param {value} Buffer with the optional value
+   * @param {type} Type of the optional without the ?
+   *
+   * @memberof Deserializer
+   * @inner
+   */
+  toOptional(value: Buffer, type: string): any {
+    if (value[0] === 0) {
+      // It's an empty optional
+      return null;
+    }
+
+    // Remove the first byte to deserialize the value, since it's not empty
+    const valueToDeserialize = value.slice(1);
+    return this.deserializeFromType(valueToDeserialize, type);
+  }
+
+  /**
    * Deserialize a signed value
    *
    * The signedData what will be deserialized is
@@ -130,7 +160,11 @@ class Deserializer {
     let size: number;
     // [len(serializedResult)][serializedResult][inputData]
     [size, signedBuffer] = unpackToInt(2, false, signedData);
-    const parsed = this.deserializeFromType(signedBuffer.slice(0, size), valueType);
+    let parsed = this.deserializeFromType(signedBuffer.slice(0, size), valueType);
+    if (valueType === 'bytes') {
+      // If the value is bytes, we should transform into hex to return the string
+      parsed = parsed.toString('hex');
+    }
     signedBuffer = signedBuffer.slice(size);
     return `${bufferToHex(signedBuffer)},${parsed},${valueType}`;
   }
