46eb94a4c30bd1c34dbda4b8864f6be6828298ad
tuliomir <tulio.mir@gmail.com>
2024-06-13 16:15:26 -0300

    fix: updates ecmaVersion

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   ],
   plugins: ['jest'],
   parserOptions: {
-    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    ecmaVersion: 2023, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
   },
   settings: {
