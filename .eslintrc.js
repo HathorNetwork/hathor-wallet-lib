76b1ec13e34673e27d5df49a1f6397cc279fb7fc
tuliomir <tulio.mir@gmail.com>
2024-06-13 12:47:24 -0300

    feat: adds eslint for ts and js files

---
+++ .eslintrc.js
@@ -0,0 +1,85 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  plugins: ['jest'],
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+      typescript: {}, // This loads <rootdir>/tsconfig.json to eslint
+    },
+  },
+  rules: {
+    'prettier/prettier': 'error', // Add Prettier errors as ESLint errors
+    'arrow-parens': ['error', 'as-needed'],
+    'import/no-named-as-default': 0,
+    'import/prefer-default-export': 0,
+    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    'comma-dangle': 0,
+    'object-curly-newline': ['error', { ObjectPattern: { multiline: true } }],
+    eqeqeq: [1, 'allow-null'],
+    'no-console': 'off',
+    'no-continue': 0,
+    'no-cond-assign': 1,
+    'no-constant-condition': 0,
+    'no-control-regex': 1,
+    'no-debugger': 1,
+    'no-dupe-keys': 1,
+    'no-ex-assign': 1,
+    'no-extra-boolean-cast': 1,
+    'no-func-assign': 1,
+    'no-regex-spaces': 1,
+    'no-unreachable': 1,
+    'no-fallthrough': 1,
+    'no-lone-blocks': 1,
+    'no-delete-var': 1,
+    'no-shadow': 1,
+    'no-shadow-restricted-names': 1,
+    'no-undef': 2,
+    'no-undef-init': 1,
+    'no-use-before-define': 0,
+    'no-unused-vars': [1, { vars: 'all', args: 'none' }],
+    'no-underscore-dangle': 0,
+    'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
+    'no-await-in-loop': 'off',
+    'no-plusplus': 'off',
+    'guard-for-in': 'off',
+    'no-bitwise': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+      },
+    },
+    {
+      // Apply Jest global variables only to test files
+      files: ['__tests__/**/*.{js,ts}'],
+      env: {
+        jest: true,
+      },
+      plugins: ['jest'],
+      extends: ['plugin:jest/recommended'], // Use recommended Jest rules
+    },
+  ],
+};
