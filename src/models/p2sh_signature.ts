12f5275d48a3fdc5d9409d89e9d0464636d591bc
tuliomir <tulio.mir@gmail.com>
2024-06-19 16:05:40 -0300

    refactor: suggested destructuring

--- src/models/p2sh_signature.ts
+++ src/models/p2sh_signature.ts
@@ -56,9 +56,8 @@ class P2SHSignature {
     const xpub = arr[0];
     const signatures: Record<number, string> = {};
     for (const sig of arr.slice(1)) {
-      const parts = sig.split(':');
-      // eslint-disable-next-line prefer-destructuring -- Destructuring would make this harder to read
-      signatures[+parts[0]] = parts[1];
+      const [key, value] = sig.split(':');
+      signatures[+key] = value;
     }
     return new P2SHSignature(xpub, signatures);
   }
