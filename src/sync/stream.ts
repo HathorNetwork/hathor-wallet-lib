fe59015cfca8f0e9c6d0b333558990c122bc1613
André Carneiro <andreluizmrcarneiro@gmail.com>
2024-07-18 12:48:33 -0400

    chore: linter

--- src/sync/stream.ts
+++ src/sync/stream.ts
@@ -94,7 +94,12 @@ export async function xpubStreamSyncHistory(
     }
   }
 
-  const manager = new StreamManager(startIndex, storage, connection, modeHistorySyncMode.XPUB_STREAM_WS);
+  const manager = new StreamManager(
+    firstIndex,
+    storage,
+    connection,
+    HistorySyncMode.XPUB_STREAM_WS
+  );
   await streamSyncHistory(manager, shouldProcessHistory);
 }
 
@@ -114,7 +119,12 @@ export async function manualStreamSyncHistory(
     }
   }
 
-  const manager = new StreamManager(startIndex, storage, connection, modeHistorySyncMode.MANUAL_STREAM_WS);
+  const manager = new StreamManager(
+    firstIndex,
+    storage,
+    connection,
+    HistorySyncMode.MANUAL_STREAM_WS
+  );
   await streamSyncHistory(manager, shouldProcessHistory);
 }
 
@@ -131,20 +141,33 @@ export class StreamManager extends AbortController {
   streamId: string;
 
   MAX_WINDOW_SIZE = 600;
+
   ADDRESSES_PER_MESSAGE = 40;
+
   UI_UPDATE_INTERVAL = 500;
 
   storage: IStorage;
+
   connection: FullNodeConnection;
+
   xpubkey: string;
+
   gapLimit: number;
+
   network: string;
+
   lastLoadedIndex: number;
+
   lastReceivedIndex: number;
+
   canUpdateUI: boolean;
+
   foundAnyTx: boolean;
+
   mode: HistorySyncMode;
+
   executionQueue: Promise<void>;
+
   batchQueue: Promise<void>;
 
   errorMessage: string | null;
@@ -159,7 +182,7 @@ export class StreamManager extends AbortController {
     startIndex: number,
     storage: IStorage,
     connection: FullNodeConnection,
-    mode: HistorySyncMode,
+    mode: HistorySyncMode
   ) {
     super();
     this.streamId = generateStreamId();
@@ -183,16 +206,20 @@ export class StreamManager extends AbortController {
    * Make initial preparations and lock the stream on the connection.
    */
   async setupStream() {
-    const accessData = await storage.getAccessData();
+    if (![HistorySyncMode.MANUAL_STREAM_WS, HistorySyncMode.XPUB_STREAM_WS].includes(this.mode)) {
+      throw new Error(`Unsupported stream mode ${this.mode}`);
+    }
+
+    const accessData = await this.storage.getAccessData();
     if (accessData === null) {
       throw new Error('No access data');
     }
     const { xpubkey } = accessData;
     // Should not throw here since we only support gapLimit wallets
-    const gapLimit = await storage.getGapLimit();
+    const gapLimit = await this.storage.getGapLimit();
     this.xpubkey = xpubkey;
     this.gapLimit = gapLimit;
-    this.network = storage.config.getNetwork().name;
+    this.network = this.storage.config.getNetwork().name;
 
     // Make sure this is the only stream running on this connection
     if (!this.connection.lockStream(this.streamId)) {
@@ -313,18 +340,28 @@ export class StreamManager extends AbortController {
         }, this.UI_UPDATE_INTERVAL);
       }
     });
-  };
+  }
 
   sendStartMessage() {
     switch (this.mode) {
       case HistorySyncMode.XPUB_STREAM_WS:
-        this.connection.sendStartXPubStreamingHistory(this.streamId, this.lastLoadedIndex + 1, this.xpubkey, this.gapLimit);
+        this.connection.sendStartXPubStreamingHistory(
+          this.streamId,
+          this.lastLoadedIndex + 1,
+          this.xpubkey,
+          this.gapLimit
+        );
         break;
       case HistorySyncMode.MANUAL_STREAM_WS:
         this.connection.sendManualStreamingHistory(
           this.streamId,
           this.lastLoadedIndex + 1,
-          loadAddressesCPUIntensive(this.lastLoadedIndex + 1, this.ADDRESSES_PER_MESSAGE, this.xpubkey, this.network),
+          loadAddressesCPUIntensive(
+            this.lastLoadedIndex + 1,
+            this.ADDRESSES_PER_MESSAGE,
+            this.xpubkey,
+            this.network
+          ),
           true,
           this.gapLimit
         );
@@ -396,12 +433,8 @@ function buildListener(manager: StreamManager, resolve: () => void) {
  */
 export async function streamSyncHistory(
   manager: StreamManager,
-  shouldProcessHistory: boolean,
+  shouldProcessHistory: boolean
 ): Promise<void> {
-  if (![HistorySyncMode.MANUAL_STREAM_WS, HistorySyncMode.XPUB_STREAM_WS].includes(mode)) {
-    throw new Error(`Unsupported stream mode ${mode}`);
-  }
-
   await manager.setupStream();
 
   // This is a try..finally so that we can always call the signal abort function
@@ -414,9 +447,14 @@ export async function streamSyncHistory(
      * - An error happens in the fullnode.
      */
     await new Promise<void>(resolve => {
-
       // If the manager aborts we need to resolve and exit the promise.
-      manager.signal.addEventListener('abort', () => {resolve();}, { once: true });
+      manager.signal.addEventListener(
+        'abort',
+        () => {
+          resolve();
+        },
+        { once: true }
+      );
       // If it is already aborted for some reason, just exit
       if (manager.signal.aborted) {
         resolve();
