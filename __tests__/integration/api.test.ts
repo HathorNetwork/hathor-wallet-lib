910752d4632e5615a77d6326af3215a5c5b7fbe0
Andre Carneiro <andreluizmrcarneiro@gmail.com>
2025-04-28 11:17:15 -0300

    chore: linter changes

--- __tests__/integration/api.test.ts
+++ __tests__/integration/api.test.ts
@@ -1,11 +1,11 @@
-import featureApi from '../../src/api/featureApi';
+import featureApi from '../../src/api/featuresApi';
 
 describe('Feature api', () => {
   it('should be able to call the features api', async () => {
     await expect(featureApi.getFeatures()).resolves.not.toThrow();
   });
 
-  it('should be able to call the features api', async () => {
+  it('should be able to call the block features api', async () => {
     const response = await featureApi.getFeatures();
     await expect(featureApi.getBlockFeatures(response.block_hash)).resolves.not.toThrow();
   });
