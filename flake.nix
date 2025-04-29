22bb0627642c380d58695679c7a16b47020b64bd
Andre Carneiro <andreluizmrcarneiro@gmail.com>
2025-04-29 13:29:06 -0300

    chore: use a more complete devshell

--- flake.nix
+++ flake.nix
@@ -1,27 +1,50 @@
 {
-  description = "Hathor wallet-lib environment";
+  description = "Hathor wallet-lib's virtual environments";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  };
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { nixpkgs, ... }:
-  let
-    supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-  in
-  {
-    devShells = forAllSystems (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        default = pkgs.mkShell {
-          buildInputs = with pkgs; [
+  outputs = { flake-utils, devshell, nixpkgs, ... }:
+
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShell =
+        let pkgs = import nixpkgs {
+          inherit system;
+
+          overlays = [ devshell.overlays.default ];
+        };
+        in
+        pkgs.devshell.mkShell {
+          commands = [
+            {
+              category = "tests";
+              name = "unit_tests";
+              help = "Run unit tests";
+              command = "make test";
+            }
+            {
+              category = "build";
+              name = "build";
+              help = "Build wallet-lib";
+              command = "npm run build";
+            }
+            {
+              category = "build";
+              name = "lint_check";
+              help = "Run lint checker";
+              command = "npm run lint";
+            }
+            {
+              category = "build";
+              name = "lint_fix";
+              help = "Run lint and fix if possible";
+              command = "npm run lint:fix";
+            }
+          ];
+          packages = with pkgs; [
+            nixpkgs-fmt
             nodejs_22
-            docker # For integration test infrastructure
           ];
         };
-      });
-  };
+    });
 }
