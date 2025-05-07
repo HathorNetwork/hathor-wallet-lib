c6afbb83a923d62677bc384797bc1c3d0d727654
Andre Carneiro <andreluizmrcarneiro@gmail.com>
2025-05-06 21:33:58 -0300

    chore: add unsigned leb128 util changes

--- src/utils/leb128.ts
+++ src/utils/leb128.ts
@@ -8,18 +8,39 @@
 export interface Leb128DecodeResult {
   value: bigint;
   rest: Buffer;
+  bytesRead: number;
 }
 
-export function encodeSigned(value: bigint | number, maxBytes: number | null = null): Buffer {
+/**
+ * Encode a number into a leb128 encoded buffer.
+ * @param value The actual value to encode.
+ * @param [signed=true] Differentiate signed and unsigned encoded numbers.
+ * @param [maxBytes=null] Max allowed size of the output buffer.
+ */
+export function encodeLeb128(
+  value: bigint | number,
+  signed: boolean = true,
+  maxBytes: number | null = null
+) {
   let val = BigInt(value);
+  if (!signed && value < 0) {
+    throw new Error('Cannot encode an unsigned negative value');
+  }
   const result: bigint[] = [];
   while (true) {
     const byte = val & 0b0111_1111n;
     val >>= 7n;
-    if (
-      (val === 0n && (byte & 0b0100_0000n) === 0n) ||
-      (val === -1n && (byte & 0b0100_0000n) !== 0n)
-    ) {
+    let isLastByte = false;
+    if (signed) {
+      // signed check for last byte
+      isLastByte =
+        (val === 0n && (byte & 0b0100_0000n) === 0n) ||
+        (val === -1n && (byte & 0b0100_0000n) !== 0n);
+    } else {
+      // unsigned check for last byte
+      isLastByte = val === 0n && (byte & 0b1000_0000n) === 0n;
+    }
+    if (isLastByte) {
       result.push(byte);
       if (maxBytes !== null && result.length > maxBytes) {
         throw new Error(`Cannot encode more than ${maxBytes} bytes`);
@@ -37,18 +58,29 @@ export function encodeSigned(value: bigint | number, maxBytes: number | null = n
   }
 }
 
-export function decodeSigned(buf: Buffer, maxBytes: number | null = null): Leb128DecodeResult {
+/**
+ * Decode a leb128 buffer into a number if possible.
+ * @param buf The buffer with the actual data.
+ * @param [signed=true] Differentiate signed and unsigned encoded numbers.
+ * @param [maxBytes=null] Max allowed size of the output buffer.
+ */
+export function decodeLeb128(
+  buf: Buffer,
+  signed: boolean = true,
+  maxBytes: number | null = null
+): Leb128DecodeResult {
   const byte_list = Array.from(buf.values()).map(v => BigInt(v));
   let result = 0n;
   let shift = 0n;
+  let bytesRead = 0;
   while (true) {
+    bytesRead += 1;
     const byte = byte_list.shift();
     if (byte === undefined) {
       throw new Error('Buffer is not valid leb128, cannot read from empty buffer');
     }
     result |= (byte & 0b0111_1111n) << shift;
     shift += 7n;
-    // assert shift % 7n === 0
     if (shift % 7n !== 0n)
       throw new Error(`AssertionError: shift is ${shift} and is not divisible by 7`);
 
@@ -58,23 +90,58 @@ export function decodeSigned(buf: Buffer, maxBytes: number | null = null): Leb12
 
     if ((byte & 0b1000_0000n) === 0n) {
       // Last byte
-      if ((byte & 0b0100_0000n) !== 0n) {
+      if (signed && (byte & 0b0100_0000n) !== 0n) {
         // Negative sign
         return {
           value: result | -(1n << shift),
           rest: Buffer.from(byte_list.map(b => Number(b))),
+          bytesRead,
         };
       }
 
       return {
         value: result,
         rest: Buffer.from(byte_list.map(b => Number(b))),
+        bytesRead,
       };
     }
   }
 }
 
+/**
+ * Encode signed leb128 number
+ */
+export function encodeSigned(value: bigint | number, maxBytes: number | null = null): Buffer {
+  return encodeLeb128(value, true, maxBytes);
+}
+
+/**
+ * Decode signed leb128 number
+ */
+export function decodeSigned(buf: Buffer, maxBytes: number | null = null): Leb128DecodeResult {
+  return decodeLeb128(buf, true, maxBytes);
+}
+
+/**
+ * Encode unsigned leb128 number
+ */
+export function encodeUnsigned(value: bigint | number, maxBytes: number | null = null): Buffer {
+  return encodeLeb128(value, false, maxBytes);
+}
+
+/**
+ * Decode unsigned leb128 number
+ */
+export function decodeUnsigned(buf: Buffer, maxBytes: number | null = null): Leb128DecodeResult {
+  return decodeLeb128(buf, false, maxBytes);
+}
+
 export default {
+  encodeLeb128,
+  decodeLeb128,
+
   encodeSigned,
   decodeSigned,
+  encodeUnsigned,
+  decodeUnsigned,
 };
