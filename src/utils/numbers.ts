03a1c5dd426bca79f8fd8ea4b2cb8ee4b846a621
André Carneiro <andreluizmrcarneiro@gmail.com>
2025-05-26 14:42:37 -0300

    feat: explorer required changes (#871)
    * feat: explorer required changes

--- src/utils/numbers.ts
+++ src/utils/numbers.ts
@@ -5,14 +5,18 @@ const formatter = new Intl.NumberFormat('en-US');
 /**
  * Get the formatted value with decimal places and thousand separators
  *
- * @param {bigint} value Amount to be formatted
- * @param {number} [decimalPlaces=DECIMAL_PLACES] Number of decimal places
+ * @param inputValue Amount to be formatted
+ * @param [decimalPlaces=DECIMAL_PLACES] Number of decimal places
  *
  * @return {string} Formatted value
  *
  * @inner
  */
-export function prettyValue(value: bigint, decimalPlaces = DECIMAL_PLACES): string {
+export function prettyValue(
+  inputValue: bigint | number | string,
+  decimalPlaces = DECIMAL_PLACES
+): string {
+  const value = BigInt(inputValue);
   if (typeof value !== 'bigint') {
     throw Error(`value ${value} should be a bigint`);
   }
