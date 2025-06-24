6fd6e5a9e5e2d74b10b15d411352653e05a55efe
Andre Carneiro <andreluizmrcarneiro@gmail.com>
2025-06-23 21:05:17 -0300

    chore: expect object containing instead of whole object

--- __tests__/new/hathorwallet.test.ts
+++ __tests__/new/hathorwallet.test.ts
@@ -1388,7 +1388,7 @@ test('build transaction template', async () => {
   const tx = await hwallet.buildTxTemplate([{ type: 'action/complete' }]);
   expect(tx).toBe(preMadeTx);
   expect(interpreter.build).toHaveBeenCalledTimes(1);
-  expect(interpreter.build).toHaveBeenCalledWith([{ type: 'action/complete' }], true);
+  expect(interpreter.build).toHaveBeenCalledWith([expect.objectContaining({ type: 'action/complete' })], true);
   expect(dataSpy).not.toHaveBeenCalled();
 });
 
@@ -1431,7 +1431,7 @@ test('build transaction template with signature', async () => {
   });
   expect(tx).toBe(preMadeTx);
   expect(interpreter.build).toHaveBeenCalledTimes(1);
-  expect(interpreter.build).toHaveBeenCalledWith([{ type: 'action/complete' }], true);
+  expect(interpreter.build).toHaveBeenCalledWith([expect.objectContaining({ type: 'action/complete' })], true);
   expect(dataSpy).toHaveBeenCalledTimes(1);
 });
 
