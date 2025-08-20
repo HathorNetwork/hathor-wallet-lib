2d01216a715364ee418936ced9fa8714fa999db8
Pedro Ferreira <pedro@hathor.network>
2025-08-20 17:44:17 -0300

    fix: check if seqnumMeta is null instead of not null

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -646,7 +646,7 @@ export async function processNewTx(
     if (callerAddressInfo) {
       // create metadata for address if it does not exist
       let seqnumMeta = await store.getSeqnumMeta(caller);
-      if (seqnumMeta != null) {
+      if (seqnumMeta === null) {
         seqnumMeta = -1;
       }
 
@@ -842,7 +842,7 @@ export async function processNewTx(
     if (callerAddressInfo && tx.nc_id && tx.nc_seqnum != null) {
       // create metadata for address if it does not exist
       let seqnumMeta = await store.getSeqnumMeta(caller);
-      if (seqnumMeta != null) {
+      if (seqnumMeta === null) {
         seqnumMeta = -1;
       }
 
