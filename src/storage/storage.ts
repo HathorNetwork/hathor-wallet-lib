7fd928ca3c563b06f3cd0dcb28306aa86c4826a1
Pedro Ferreira <pedro@hathor.network>
2025-08-20 21:10:25 -0300

    refactor: getAddressInfo should return default seqnum if it's null

--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -210,17 +210,17 @@ export class Storage implements IStorage {
    *
    * @param {string} base58 The base58 address to fetch
    * @async
-   * @returns {Promise<(IAddressInfo & Partial<IAddressMetadata> & { seqnum: number | null })|null>} The address info or null if not found
+   * @returns {Promise<(IAddressInfo & Partial<IAddressMetadata> & { seqnum: number })|null>} The address info or null if not found
    */
   async getAddressInfo(
     base58: string
-  ): Promise<(IAddressInfo & IAddressMetadata & { seqnum: number | null }) | null> {
+  ): Promise<(IAddressInfo & IAddressMetadata & { seqnum: number }) | null> {
     const address = await this.store.getAddress(base58);
     if (address === null) {
       return null;
     }
     const meta = await this.store.getAddressMeta(base58);
-    const seqnum = await this.store.getSeqnumMeta(base58);
+    const seqnum = await this.store.getSeqnumMeta(base58) ?? -1;
     return { ...address, ...DEFAULT_ADDRESS_META, ...meta, seqnum };
   }
 
