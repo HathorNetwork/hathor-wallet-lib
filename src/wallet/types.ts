c258a5e16eaefa0f707ba63b0f62cd0702e7a8c0
Pedro Ferreira <pedro@hathor.network>
2025-08-22 08:41:03 -0300

    feat: add getServerUrl to wallet service facade (#908)

--- src/wallet/types.ts
+++ src/wallet/types.ts
@@ -354,6 +354,7 @@ export interface IHathorWallet {
   checkPin(pin: string): Promise<boolean>;
   checkPassword(password: string): Promise<boolean>;
   checkPinAndPassword(pin: string, password: string): Promise<boolean>;
+  getServerUrl(): string;
 }
 
 export interface ISendTransaction {
