600d61ebcc8206c830db24b14e4ac7b92e4e0a09
tuliomir <tulio.mir@gmail.com>
2025-09-02 19:59:31 -0300

    chore: simplifying initial test suite

--- __tests__/integration/walletservice_facade.test.ts
+++ __tests__/integration/walletservice_facade.test.ts
@@ -1,9 +1,4 @@
-import axios, { InternalAxiosRequestConfig } from 'axios';
-import Network from '../../src/models/network';
-import { MemoryStore, Storage } from '../../src';
-import walletUtils from '../../src/utils/wallet';
-import { WALLET_CONSTANTS } from './configuration/test-constants';
-import HathorWalletServiceWallet from '../../src/wallet/wallet';
+import axios from 'axios';
 import config from '../../src/config';
 import { loggers } from './utils/logger.util';
 
@@ -29,38 +24,5 @@ describe('version', () => {
       });
     expect(response.status).toBe(200);
     expect(response.data?.success).toBe(true);
-  });
-});
-
-const defaultPinPass = '1234';
-describe('start', () => {
-  it.skip('should start the wallet service facade without errors', async () => {
-    const requestPassword = jest.fn();
-    const network = new Network('privatenet');
-    const seed = WALLET_CONSTANTS.genesis.words;
-    const store = new MemoryStore();
-    const storage = new Storage(store);
-    const accessData = walletUtils.generateAccessDataFromSeed(seed, {
-      networkName: 'privatenet',
-      password: defaultPinPass,
-      pin: defaultPinPass,
-    });
-
-    // Create an instance of the WalletServiceFacade
-    const wallet = new HathorWalletServiceWallet({
-      requestPassword,
-      seed,
-      network,
-      storage,
-    });
-
-    // Call the start method and assert no errors are thrown
-    await expect(wallet.start()).rejects.toThrow('Pin code');
-
-    // Call the start method with pin and password and assert no errors are thrown
-    await wallet.start({ pinCode: defaultPinPass, password: defaultPinPass });
-    await expect(wallet.storage.getAccessData()).resolves.toMatchObject({
-      xpubkey: accessData.xpubkey,
-    });
   });
 });
