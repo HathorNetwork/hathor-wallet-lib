62bbda5c54dbe35cc788935327951f0c3c9d8fab
André Carneiro <andreluizmrcarneiro@gmail.com>
2025-09-19 09:45:25 -0300

    chore: add hathor header to files

--- src/sync/gll.ts
+++ src/sync/gll.ts
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) Hathor Labs and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import PromiseQueue from '../models/promise_queue';
 
 const MAX_CONCURRENT_LOAD_TASKS = 3;
--- src/sync/stream.ts
+++ src/sync/stream.ts
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) Hathor Labs and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import { Address as BitcoreAddress, HDPublicKey } from 'bitcore-lib';
 import queueMicrotask from 'queue-microtask';
 import FullNodeConnection from '../new/connection';
