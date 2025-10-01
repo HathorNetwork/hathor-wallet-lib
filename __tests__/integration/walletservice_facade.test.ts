1b272013d6390fd81dd9cd303a3064a0a4d758de
tuliomir <tulio.mir@gmail.com>
2025-10-01 16:44:34 -0300

    test: helper functions and structure

--- __tests__/integration/walletservice_facade.test.ts
+++ __tests__/integration/walletservice_facade.test.ts
@@ -1,28 +1,281 @@
 import axios from 'axios';
+import Mnemonic from 'bitcore-mnemonic';
 import config from '../../src/config';
 import { loggers } from './utils/logger.util';
+import HathorWalletServiceWallet from '../../src/wallet/wallet';
+import Network from '../../src/models/network';
+import { CreateTokenTransaction, MemoryStore, Output, Storage } from '../../src';
+import {
+  FULLNODE_NETWORK_NAME,
+  FULLNODE_URL,
+  NETWORK_NAME,
+  WALLET_CONSTANTS,
+} from './configuration/test-constants';
+import {
+  TOKEN_MELT_MASK,
+  TOKEN_MINT_MASK,
+  WALLET_SERVICE_AUTH_DERIVATION_PATH,
+} from '../../src/constants';
+import { decryptData } from '../../src/utils/crypto';
+import walletUtils from '../../src/utils/wallet';
+import { delay } from './utils/core.util';
+import { TxNotFoundError, UtxoError, WalletRequestError } from '../../src/errors';
+import { NATIVE_TOKEN_UID } from '../../lib/constants';
+import { GetAddressesObject } from '../../lib/wallet/types';
 
 // Set base URL for the wallet service API inside the privatenet test container
 config.setWalletServiceBaseUrl('http://localhost:3000/dev/');
-config.setWalletServiceBaseWsUrl('ws://localhost:3001/dev/');
+config.setWalletServiceBaseWsUrl('ws://localhost:3001/');
 
-describe('version', () => {
-  it('should retrieve the version data', async () => {
-    const response = await axios
-      .get('version', {
-        baseURL: config.getWalletServiceBaseUrl(),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-      .catch(e => {
-        loggers.test.log(`Received an error on /version: ${e}`);
-        if (e.response) {
-          return e.response;
-        }
-        throw e;
-      });
-    expect(response.status).toBe(200);
-    expect(response.data?.success).toBe(true);
+/** Genesis Wallet, used to fund all tests */
+const gWallet: HathorWalletServiceWallet = buildWalletInstance({
+  words: WALLET_CONSTANTS.genesis.words,
+}).wallet;
+/** Wallet instance used in tests */
+let wallet: HathorWalletServiceWallet;
+const emptyWallet = {
+  words:
+    'buddy kingdom scorpion device uncover donate sense false few leaf oval illegal assume talent express glide above brain end believe abstract will marine crunch',
+  addresses: [
+    'WkHNZyrKNusTtu3EHfvozEqcBdK7RoEMR7',
+    'WivGyxDjWxijcns3hpGvEJKhjR9HMgFzZ5',
+    'WXQSeMcNt67hVpmgwYqmYLsddgXeGYP4mq',
+    'WTMH3NQs8YXyNguqwLyqoTKDFTfkJLxMzX',
+    'WTUiHeiajtt1MXd1Jb3TEeWUysfNJfig35',
+    'WgzZ4MNcuX3sBgLC5Fa6dTTQaoy4ccLdv5',
+    'WU6UQCnknGLh1WP392Gq6S69JmheS5kzZ2',
+    'WX7cKt38FfgKFWFxSa2YzCWeCPgMbRR98h',
+    'WZ1ABXsuwHHfLzeAWMX7RYs5919LPBaYpp',
+    'WUJjQGb4SGSLh44m2JdgAR4kui8mTPb8bK',
+  ],
+};
+const walletWithTxs = {
+  words:
+    'bridge balance milk impact love orchard achieve matrix mule axis size hip cargo rescue truth stable setup problem nerve fit million manage harbor connect',
+  addresses: [
+    'WeSnE5dnrciKahKbTvbUWmY6YM9Ntgi6MJ',
+    'Wj52SGubNZu3JA2ncRXyNGfqyrdnj4XTU2',
+    'Wh1Xs7zPVT9bc6dzzA23Zu8aiP3H8zLkiy',
+    'WdFeZvVJkwAdLDpXLGJpFP9XSDcdrntvAg',
+    'WTdWsgnCPKBuzEKAT4NZkzHaD4gHYMrk4G',
+    'WSBhEBkuLpqu2Fz1j6PUyUa1W4GGybEYSF',
+    'WS8yocEYBykpgjQxAhxjTcjVw9gKtYdys8',
+    'WmkBa6ikYM2sZmiopM6zBGswJKvzs5Noix',
+    'WeEPszSx14og6c3uPXy2vYh7BK9c6Zb9TX',
+    'WWrNhymgFetPfxCv4DncveG6ykLHspHQxv',
+  ],
+};
+const customTokenWallet = {
+  words:
+    'shine myself welcome feature nurse cement crumble input utility lizard melt great sample slab know leisure salmon path gate iron enlist discover cry radio',
+  addresses: [
+    'WUTMZMaNoewWprYpb8b2etTfRuw2zRS5u3',
+    'WNxz1juhoJk9Y28knsH8ynVXn9s7bYLMkd',
+    'WWBkdHcB7TCjNQUiyFhZpXDU4Tejd7AFd5',
+    'WdcFTovRGiePVSnCpoGUYBgAGyDmXhkaD6',
+    'WjuxR4r487CTGGMJRpcnAZ9bdaH91qt7F5',
+    'WkYJ6SHfS2CTAMCxtrwWm7Mr12YRMx7WzQ',
+    'WTuBXE1WPNgjSxqDfRKy2fiDydT3e2pdiJ',
+    'WTipscpJ14sAZ4Y3f2gY5Tb1RWJYop9QYK',
+    'WhQovEXRSDc7MLMz8Tqy1qJdTy2hef1dYq',
+    'WkUREDxNQX6Qq1NwdLQycxkHFjKujQasWX',
+  ],
+};
+const multipleTokensWallet = {
+  words:
+    'object join brain round loyal unfair shine genius brain vocal object crouch simple cake chase october unlock detail ivory kidney saddle immense deer response',
+  addresses: [
+    'Wie8wTxa7P6Vbr1UhADfDfafJftyYsZNMU',
+    'WaCk6XV4zCwdPTvGH6VgkE58ebqEndA6b7',
+    'Wdsez9n6LuWMtKQv3zdnKtQkTeXFw7ATFj',
+    'WdZcUpCoLS1CK5UD7V5Z4d42X92zc7QHEi',
+    'WaaXsw2HdYiBUveqg6QWS5HmkwTNUKUBLD',
+    'WPxVMXd89aaXWXqUcVTjdQPUvuT3wehJxc',
+    'WSbfhb9tkJneSbEJsyzyEURYcTqkPKRUUD',
+    'WQt8Gxy5yWC3xZGsHVrywYJqHyg5xtudun',
+    'WcpnSRvzZGAnR6rtQiBnzP7aLnvPstpXbD',
+    'WQzooroUKJMrFVv5P1UrPppmQ2YF8ACfAS',
+  ],
+};
+const addressesWallet = {
+  words:
+    'pumpkin tank father organ can doll romance damage because barely vault pride will man rack horn lamp remove enemy brain desert exchange boil salon',
+  addresses: [
+    'WRsDG9VhM4N9DPSpbnpFKnngLEXonaBsuH',
+    'WSTMdCz4BuzGv5q6g8woaCHeyppTZdjXWx',
+    'WPbCV3Lrh28ntoQY2hvC2ppU5TimCZdRaw',
+    'WaAgCebJjWfQCKcDwtpffQ4kt2im7fbsUr',
+    'WXN2wRybweJY4xunPkz6pwfGUmoumCCcUP',
+    'WbEA4E7Rnx98TtRox3UazMQRm1yNoAJcfm',
+    'WZs2Ci9ZxyMzmfdbGfR2nTp9xsxS7rSsDN',
+    'Wf1waSNgXmMoitFjx7TADMemKyCWjhvLUb',
+    'WVTMecGGC9kGzUbQqjB4J7i4KVhLVyMagy',
+    'WjHom47afCW8qEFtBqMq3MT22zxLkuvQag',
+  ],
+};
+const utxosWallet = {
+  words:
+    'provide bunker age agree renew size popular license best kidney range flag they bulk survey letter concert mobile february clean nuclear inherit voyage capable',
+  addresses: [
+    'WQvAdYAqZf69nsgzVwSMwfRWcBRHJJU1qH',
+    'We4fZtzxod2M3w1u8h4TNpaMYrYWqXxNqd',
+    'WioaJZPzytLVniJ9MTinLiWih1VaoRfaUV',
+    'WmRLJj5P1rj1bErNADJnweq8mXBNLmNiAL',
+    'WXpXoREmV2hFuMX83dup7YMqJqRW5Y94Av',
+    'WirQUza1XdqnN7DcAMdXvysTntq9DB3xz6',
+    'Wb26hUGD6du7nkecrAeaRbBoZS4Z3dynby',
+    'WXgFTQm7uNYTj8gsz3GWNg58jCvaPn96hD',
+    'WdcFv1fKjbPPqSXHkdo22QE2bbZnbXADHK',
+    'WTm47mTSd7ompdinkZM3LiF4VE7AeQttzo',
+  ],
+};
+
+/** Default pin to simplify the tests */
+const pinCode = '123456';
+/** Default password to simplify the tests */
+const password = 'testpass';
+
+/**
+ * Builds a HathorWalletServiceWallet instance with a wallet seed words
+ * @param enableWs - Whether to enable websocket connection (default: false)
+ * @param words - The 24 words to use for the wallet (default: empty wallet)
+ * @param passwordForRequests - The password that will be returned by the mocked requestPassword function (default: 'test-password')
+ * @returns The wallet instance along with its store and storage for eventual mocking/spying
+ */
+function buildWalletInstance({
+  enableWs = false,
+  words = emptyWallet.words,
+  passwordForRequests = 'test-password',
+} = {}) {
+  const walletData = { words };
+  const network = new Network(NETWORK_NAME);
+  const requestPassword = jest.fn().mockResolvedValue(passwordForRequests);
+
+  const store = new MemoryStore();
+  const storage = new Storage(store);
+  const newWallet = new HathorWalletServiceWallet({
+    requestPassword,
+    seed: walletData.words,
+    network,
+    storage,
+    enableWs, // Disable websocket for integration tests
   });
+
+  return { wallet: newWallet, store, storage };
+}
+
+/**
+ * Polls the wallet for a transaction by its ID until found or max attempts reached
+ * @param walletForPolling - The wallet instance to poll
+ * @param txId - The transaction ID to look for
+ * @returns The transaction object if found
+ * @throws Error if the transaction is not found after max attempts
+ */
+async function poolForTx(walletForPolling: HathorWalletServiceWallet, txId: string) {
+  const maxAttempts = 10;
+  const delayMs = 1000; // 1 second
+  let attempts = 0;
+
+  while (attempts < maxAttempts) {
+    try {
+      const tx = await walletForPolling.getTxById(txId);
+      if (tx) {
+        loggers.test!.log(`Pooling for ${txId} took ${attempts + 1} attempts`);
+        return tx;
+      }
+    } catch (error) {
+      // If the error is of type TxNotFoundError, we continue polling
+      if (!(error instanceof TxNotFoundError)) {
+        throw error; // Re-throw unexpected errors
+      }
+    }
+    attempts++;
+    await delay(delayMs);
+  }
+  throw new Error(`Transaction ${txId} not found after ${maxAttempts} attempts`);
+}
+
+async function generateNewWalletAddress() {
+  const newWords = walletUtils.generateWalletWords();
+  const { wallet: newWallet } = buildWalletInstance({ words: newWords });
+  await newWallet.start({ pinCode, password });
+
+  const addresses: string[] = [];
+  for (let i = 0; i < 10; i++) {
+    addresses.push((await newWallet.getAddressAtIndex(i))!);
+  }
+
+  return {
+    words: newWords,
+    addresses,
+  };
+}
+
+async function sendFundTx(
+  address: string,
+  amount: bigint,
+  destinationWallet?: HathorWalletServiceWallet
+) {
+  const fundTx = await gWallet.sendTransaction(address, amount, {
+    pinCode,
+  });
+
+  // Ensure the transaction was sent from the Genesis perspective
+  await poolForTx(gWallet, fundTx.hash!);
+
+  // Ensure the destination wallet is also aware of the transaction
+  if (destinationWallet) {
+    await poolForTx(destinationWallet, fundTx.hash!);
+  }
+
+  return fundTx;
+}
+
+beforeAll(async () => {
+  console.log(`${JSON.stringify(await generateNewWalletAddress(), null, 2)}`);
+
+  let isServerlessReady = false;
+  const startTime = Date.now();
+
+  // Pool for the serverless app to be ready.
+  const delayBetweenRequests = 3000;
+  const lambdaTimeout = 30000;
+  while (isServerlessReady) {
+    try {
+      // Executing a method that does not depend on the wallet being started,
+      // but that ensures the Wallet Service Lambdas are receiving requests
+      await gWallet.getVersionData();
+      isServerlessReady = true;
+    } catch (e) {
+      // Ignore errors, serverless app is probably not ready yet
+      loggers.test!.log('Ws-Serverless not ready yet, retrying in 3 seconds...');
+    }
+
+    // Timeout after 2 minutes
+    if (Date.now() - startTime > lambdaTimeout) {
+      throw new Error('Ws-Serverless did not become ready in time');
+    }
+    await delay(delayBetweenRequests);
+  }
+  await gWallet.start({ pinCode, password });
 });
+
+afterAll(async () => {
+  await gWallet.stop({ cleanStorage: true });
+});
+
+describe.skip('start', () => {});
+
+describe.skip('wallet public methods', () => {});
+
+describe.skip('empty wallet address methods', () => {});
+
+describe.skip('basic transaction methods', () => {});
+
+describe.skip('websocket events', () => {});
+
+describe.skip('balances', () => {});
+
+describe.skip('address management methods', () => {});
+
+describe.skip('getUtxos, getUtxosForAmount, getAuthorityUtxos', () => {});
