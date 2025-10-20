3a24dad4f8dc327879b52aa0f51de8524d5f6ca2
Pedro Ferreira <pedro@hathor.network>
2025-10-20 17:44:37 -0300

    tests: update integration tests blueprints for the new core sdk

--- __tests__/integration/configuration/blueprints/test_parent.py
+++ __tests__/integration/configuration/blueprints/test_parent.py
@@ -24,9 +24,9 @@ class TooManyActions(NCFail):
 
 @export
 class TestParentBlueprint(Blueprint):
-    last_created_token: TokenUid
+    last_created_token: TokenUid | None
 
-    last_created_contract: ContractId
+    last_created_contract: ContractId | None
 
     def _get_action(self, ctx: Context) -> NCAction:
         """Return the only action available; fails otherwise."""
@@ -36,7 +36,8 @@ class TestParentBlueprint(Blueprint):
 
     @public
     def initialize(self, ctx: Context) -> None:
-        pass
+        self.last_created_token = None
+        self.last_created_contract = None
 
     @public(allow_deposit=True)
     def deposit(self, ctx: Context) -> None:
@@ -45,9 +46,9 @@ class TestParentBlueprint(Blueprint):
 
     @public
     def create_token(self, ctx: Context, name: str, symbol: str, amount: Amount, mint_authority: bool, melt_authority: bool) -> None:
-        self.last_created_token = self.syscall.create_deposit_token(name, symbol, amount, mint_authority, melt_authority)
+        self.last_created_token = self.syscall.create_deposit_token(token_name=name, token_symbol=symbol, amount=amount, mint_authority=mint_authority, melt_authority=melt_authority)
  
     @public
     def create_child_contract(self, ctx: Context, blueprint_id: BlueprintId, salt: bytes, contract_name: str) -> None:
-        return_tuple = self.syscall.create_contract(blueprint_id, salt, [], contract_name)
+        return_tuple = self.syscall.setup_new_contract(blueprint_id, salt=salt).initialize(contract_name)
         self.last_created_contract = return_tuple[0]
