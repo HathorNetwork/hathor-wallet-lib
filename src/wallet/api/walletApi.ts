0a7a2e59b186d113f03109cb2ba74d82a2315e20
tuliomir <tulio.mir@gmail.com>
2025-12-08 13:42:46 -0300

    docs: explains 404 issue

--- src/wallet/api/walletApi.ts
+++ src/wallet/api/walletApi.ts
@@ -344,7 +344,8 @@ const walletApi = {
       return parseSchema(response.data, txByIdResponseSchema);
     }
 
-    // The server also might return a 404 if the tx is not found. Checking its data too
+    // A serverless-offline instance may return a 404 with an error body. In those cases
+    // we pass the response data to the guard for additional validations.
     if (response.status === 404 && response.data) {
       walletApi._txNotFoundGuard(response.data);
       throw new WalletRequestError('Error getting transaction by its id.', {
