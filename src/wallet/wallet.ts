9b045be121bd3e6583691d01ffab6cc968e59307
Tulio Miranda <tulio.mir@gmail.com>
2025-12-12 12:17:54 -0300

    chore: adds error console on async auth token fail

--- src/wallet/wallet.ts
+++ src/wallet/wallet.ts
@@ -1184,7 +1184,8 @@ class HathorWalletServiceWallet extends EventEmitter implements IHathorWallet {
       this.authToken = data.token;
     } catch (err) {
       // We should not throw here since this method is called in a fire-and-forget manner
-      // TODO: When this wallet has a logger, we should log this error to help with debugging
+      // eslint-disable-next-line no-console
+      console.error(`Error renewing auth token asynchronously: ${err}`);
       this.authToken = null;
     }
   }
