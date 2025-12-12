b1c66ad94b42f7752f71a9a7b0d7a9d897eed999
André Carneiro <andreluizmrcarneiro@gmail.com>
2025-12-12 13:13:30 -0300

    chore: move testes around

--- __tests__/integration/hathorwallet_others.test.ts
+++ __tests__/integration/hathorwallet_others.test.ts
@@ -1614,94 +1614,6 @@ describe('getAuthorityUtxos', () => {
   });
 });
 
-// This section tests methods that have side effects impacting the whole wallet. Executing it last.
-describe('internal methods', () => {
-  /** @type HathorWallet */
-  let gWallet;
-  /** @type HathorWallet */
-  let hWallet;
-  beforeAll(async () => {
-    const { hWallet: ghWallet } = await GenesisWalletHelper.getSingleton();
-    gWallet = ghWallet;
-    hWallet = await generateWalletHelper();
-  });
-
-  afterAll(async () => {
-    hWallet.stop();
-    await GenesisWalletHelper.clearListeners();
-    await gWallet.stop();
-  });
-
-  it('should test the debug methods', async () => {
-    expect(gWallet.debug).toStrictEqual(false);
-
-    gWallet.enableDebugMode();
-    expect(gWallet.debug).toStrictEqual(true);
-
-    gWallet.disableDebugMode();
-    expect(gWallet.debug).toStrictEqual(false);
-  });
-
-  it('should test network-related methods', async () => {
-    // GetServerUrl fetching from the live fullnode connection
-    expect(await gWallet.getServerUrl()).toStrictEqual(FULLNODE_URL);
-    expect(await gWallet.getNetwork()).toStrictEqual(NETWORK_NAME);
-    expect(await gWallet.getNetworkObject()).toMatchObject({
-      name: NETWORK_NAME,
-      versionBytes: { p2pkh: 73, p2sh: 135 }, // Calculated for the privnet.py config file
-      bitcoreNetwork: {
-        name: expect.stringContaining(NETWORK_NAME),
-        alias: 'test', // this is the alias for the testnet network
-        pubkeyhash: 73,
-        scripthash: 135,
-      },
-    });
-
-    // GetVersionData fetching from the live fullnode server
-    expect(await gWallet.getVersionData()).toMatchObject({
-      timestamp: expect.any(Number),
-      version: expect.any(String),
-      network: FULLNODE_NETWORK_NAME,
-      minWeight: expect.any(Number),
-      minTxWeight: expect.any(Number),
-      minTxWeightCoefficient: expect.any(Number),
-      minTxWeightK: expect.any(Number),
-      tokenDepositPercentage: 0.01,
-      rewardSpendMinBlocks: expect.any(Number),
-      maxNumberInputs: 255,
-      maxNumberOutputs: 255,
-    });
-  });
-
-  it('should change servers', async () => {
-    // Changing from our integration test privatenet to the testnet
-    gWallet.changeServer('https://node1.testnet.hathor.network/v1a/');
-    const serverChangeTime = Date.now().valueOf();
-    await delay(100);
-
-    // Validating the server change with getVersionData
-    let networkData = await gWallet.getVersionData();
-    expect(networkData.timestamp).toBeGreaterThan(serverChangeTime);
-    expect(networkData.network).toMatch(/^testnet.*/);
-
-    await gWallet.changeServer(FULLNODE_URL);
-    await delay(100);
-
-    // Reverting to the privatenet
-    networkData = await gWallet.getVersionData();
-    expect(networkData.timestamp).toBeGreaterThan(serverChangeTime + 200);
-    expect(networkData.network).toStrictEqual(FULLNODE_NETWORK_NAME);
-  });
-
-  it('should reload the storage', async () => {
-    await GenesisWalletHelper.injectFunds(hWallet, await hWallet.getAddressAtIndex(0), 10n);
-    const spy = jest.spyOn(hWallet.storage, 'processHistory');
-    // Simulate that we received an event of the connection becoming active
-    await hWallet.onConnectionChangedState(ConnectionState.CONNECTED);
-    expect(spy).toHaveBeenCalledTimes(1);
-  });
-});
-
 describe('index-limit address scanning policy', () => {
   /** @type HathorWallet */
   let hWallet;
@@ -1804,5 +1716,93 @@ describe('single address scanning policy', () => {
         }),
       ])
     );
+  });
+});
+
+// This section tests methods that have side effects impacting the whole wallet. Executing it last.
+describe('internal methods', () => {
+  /** @type HathorWallet */
+  let gWallet;
+  /** @type HathorWallet */
+  let hWallet;
+  beforeAll(async () => {
+    const { hWallet: ghWallet } = await GenesisWalletHelper.getSingleton();
+    gWallet = ghWallet;
+    hWallet = await generateWalletHelper();
+  });
+
+  afterAll(async () => {
+    hWallet.stop();
+    await GenesisWalletHelper.clearListeners();
+    await gWallet.stop();
+  });
+
+  it('should test the debug methods', async () => {
+    expect(gWallet.debug).toStrictEqual(false);
+
+    gWallet.enableDebugMode();
+    expect(gWallet.debug).toStrictEqual(true);
+
+    gWallet.disableDebugMode();
+    expect(gWallet.debug).toStrictEqual(false);
+  });
+
+  it('should test network-related methods', async () => {
+    // GetServerUrl fetching from the live fullnode connection
+    expect(await gWallet.getServerUrl()).toStrictEqual(FULLNODE_URL);
+    expect(await gWallet.getNetwork()).toStrictEqual(NETWORK_NAME);
+    expect(await gWallet.getNetworkObject()).toMatchObject({
+      name: NETWORK_NAME,
+      versionBytes: { p2pkh: 73, p2sh: 135 }, // Calculated for the privnet.py config file
+      bitcoreNetwork: {
+        name: expect.stringContaining(NETWORK_NAME),
+        alias: 'test', // this is the alias for the testnet network
+        pubkeyhash: 73,
+        scripthash: 135,
+      },
+    });
+
+    // GetVersionData fetching from the live fullnode server
+    expect(await gWallet.getVersionData()).toMatchObject({
+      timestamp: expect.any(Number),
+      version: expect.any(String),
+      network: FULLNODE_NETWORK_NAME,
+      minWeight: expect.any(Number),
+      minTxWeight: expect.any(Number),
+      minTxWeightCoefficient: expect.any(Number),
+      minTxWeightK: expect.any(Number),
+      tokenDepositPercentage: 0.01,
+      rewardSpendMinBlocks: expect.any(Number),
+      maxNumberInputs: 255,
+      maxNumberOutputs: 255,
+    });
+  });
+
+  it('should change servers', async () => {
+    // Changing from our integration test privatenet to the testnet
+    gWallet.changeServer('https://node1.testnet.hathor.network/v1a/');
+    const serverChangeTime = Date.now().valueOf();
+    await delay(100);
+
+    // Validating the server change with getVersionData
+    let networkData = await gWallet.getVersionData();
+    expect(networkData.timestamp).toBeGreaterThan(serverChangeTime);
+    expect(networkData.network).toMatch(/^testnet.*/);
+
+    await gWallet.changeServer(FULLNODE_URL);
+    await delay(100);
+
+    // Reverting to the privatenet
+    networkData = await gWallet.getVersionData();
+    expect(networkData.timestamp).toBeGreaterThan(serverChangeTime + 200);
+    expect(networkData.network).toStrictEqual(FULLNODE_NETWORK_NAME);
+  });
+
+  it('should reload the storage', async () => {
+    await GenesisWalletHelper.injectFunds(hWallet, await hWallet.getAddressAtIndex(0), 10n);
+    const spy = jest.spyOn(hWallet.storage, 'processHistory');
+    // Simulate that we received an event of the connection becoming active
+    await hWallet.onConnectionChangedState(ConnectionState.CONNECTED);
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 });
