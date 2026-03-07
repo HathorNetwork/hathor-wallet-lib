19137491cc73d438adbec2fdcba09a6e09af20ea
Tulio Miranda <tulio.mir@gmail.com>
2026-03-06 23:24:17 -0300

    chore: removes duplicated tests

--- __tests__/integration/walletservice_facade.test.ts
+++ __tests__/integration/walletservice_facade.test.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
-import Mnemonic from 'bitcore-mnemonic';
 import config from '../../src/config';
 import { loggers } from './utils/logger.util';
 import HathorWalletServiceWallet from '../../src/wallet/wallet';
-import Network from '../../src/models/network';
 import { CreateTokenTransaction, FeeHeader, Output, Storage, transactionUtils } from '../../src';
 import {
   FULLNODE_NETWORK_NAME,
@@ -15,10 +13,7 @@ import {
   NATIVE_TOKEN_UID,
   TOKEN_MELT_MASK,
   TOKEN_MINT_MASK,
-  WALLET_SERVICE_AUTH_DERIVATION_PATH,
 } from '../../src/constants';
-import { decryptData } from '../../src/utils/crypto';
-import walletUtils from '../../src/utils/wallet';
 import {
   buildWalletInstance,
   emptyWallet,
@@ -168,151 +163,6 @@ beforeAll(async () => {
 
 afterAll(async () => {
   await GenesisWalletServiceHelper.stop();
-});
-
-describe('start', () => {
-  describe('mandatory parameters validation', () => {
-    beforeEach(() => {
-      ({ wallet } = buildWalletInstance({ words: emptyWallet.words }));
-    });
-
-    afterEach(async () => {
-      if (wallet) {
-        await wallet.stop({ cleanStorage: true });
-      }
-    });
-
-    it('should throw error when pinCode is not provided', async () => {
-      await expect(wallet.start({})).rejects.toThrow(
-        'Pin code is required when starting the wallet.'
-      );
-    });
-
-    it('should throw error when password is not provided for new wallet from seed', async () => {
-      await expect(wallet.start({ pinCode })).rejects.toThrow(
-        'Password is required when starting the wallet from the seed.'
-      );
-    });
-  });
-
-  describe('handling internal errors', () => {
-    const events: string[] = [];
-    let storage: Storage;
-
-    beforeEach(() => {
-      ({ wallet, storage } = buildWalletInstance({ words: emptyWallet.words }));
-
-      // Clear events array
-      events.length = 0;
-
-      // Listen for state events
-      wallet.on('state', state => {
-        events.push(`state:${state}`);
-      });
-    });
-
-    afterEach(async () => {
-      if (wallet) {
-        await wallet.stop({ cleanStorage: true });
-      }
-    });
-
-    it('should handle getAccessData unexpected errors', async () => {
-      // XXX: This test belongs to the unit tests, but adding it here temporarily for coverage
-      jest.spyOn(storage, 'getAccessData').mockRejectedValueOnce(new Error('Crash'));
-
-      // Start the wallet
-      await expect(() => wallet.start({ pinCode, password })).rejects.toThrow('Crash');
-
-      // Verify wallet is ready
-      expect(wallet.isReady()).toBe(false);
-    });
-  });
-
-  describe('successful wallet creation', () => {
-    const events: string[] = [];
-    let storage: Storage;
-
-    beforeEach(() => {
-      ({ wallet, storage } = buildWalletInstance({ words: emptyWallet.words }));
-
-      // Clear events array
-      events.length = 0;
-
-      // Listen for state events
-      wallet.on('state', state => {
-        events.push(`state:${state}`);
-      });
-    });
-
-    afterEach(async () => {
-      if (wallet) {
-        await wallet.stop({ cleanStorage: true });
-      }
-    });
-
-    it('should create wallet with words and emit correct state events', async () => {
-      // Start the wallet
-      await wallet.start({ pinCode, password });
-
-      // Verify wallet is ready
-      expect(wallet.isReady()).toBe(true);
-
-      // Verify correct state transitions occurred
-      expect(events).toContain('state:Loading');
-      expect(events).toContain('state:Ready');
-
-      // Verify wallet has correct network
-      expect(wallet.getNetwork()).toBe(NETWORK_NAME);
-
-      // Verify wallet has addresses available
-      const currentAddress = wallet.getCurrentAddress();
-      expect(currentAddress.index).toBeDefined();
-      expect(currentAddress.address).toEqual(emptyWallet.addresses[currentAddress.index]);
-
-      // Verify websocket is disabled for this test
-      expect(wallet.isWsEnabled()).toBe(false);
-    });
-
-    it('should create wallet with xpriv', async () => {
-      // Generate access data to get the xpriv
-      const seed = emptyWallet.words;
-      const accessData = walletUtils.generateAccessDataFromSeed(seed, {
-        networkName: 'testnet',
-        password: '1234',
-        pin: '1234',
-      });
-
-      // Derive auth xpriv and account key
-      const code = new Mnemonic(seed);
-      const xpriv = code.toHDPrivateKey('', new Network('testnet'));
-      const authxpriv = xpriv.deriveChild(WALLET_SERVICE_AUTH_DERIVATION_PATH).xprivkey;
-      const acctKey = decryptData(accessData.acctPathKey!, '1234');
-
-      // Build wallet with xpriv and authxpriv
-      const network = new Network(NETWORK_NAME);
-      const requestPassword = jest.fn().mockResolvedValue('test-password');
-      wallet = new HathorWalletServiceWallet({
-        requestPassword,
-        xpriv: acctKey,
-        authxpriv,
-        network,
-        storage,
-        enableWs: false, // Disable websocket for integration tests
-      });
-
-      // Start the wallet
-      await wallet.start({ pinCode, password });
-
-      // Verify wallet is ready
-      expect(wallet.isReady()).toBe(true);
-
-      // Verify wallet has addresses available
-      const currentAddress = wallet.getCurrentAddress();
-      expect(currentAddress.index).toBeDefined();
-      expect(currentAddress.address).toEqual(emptyWallet.addresses[currentAddress.index]);
-    });
-  });
 });
 
 describe('wallet public methods', () => {
