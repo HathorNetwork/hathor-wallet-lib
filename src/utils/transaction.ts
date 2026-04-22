b9128aca3c1d529bf4c627401bb3802033798d64
Pedro Ferreira <pedro@hathor.network>
2026-04-22 18:26:05 -0300

    chore: fix prettier formatting in transaction.ts

--- src/utils/transaction.ts
+++ src/utils/transaction.ts
@@ -1086,10 +1086,7 @@ const transaction = {
       if (this.isShieldedInputEntry(i)) {
         return i as unknown as IHistoryInput;
       }
-      const hydratedInput = this.hydrateIOWithToken(
-        i as { token_data: number },
-        tx.tokens
-      );
+      const hydratedInput = this.hydrateIOWithToken(i as { token_data: number }, tx.tokens);
       return hydratedInput as IHistoryInput;
     });
     const outputs: IHistoryOutput[] = tx.outputs.map(o => {
